025633027222674108c7054538e1ea9543ed0c9b
lizzyagibson <eag2186@cumc.columbia.edu>
2019-06-21 12:31:43 -0400

    Add simulated CSV.

--- simulate_data.R
+++ simulate_data.R
@@ -11,10 +11,12 @@ library(gridExtra)
 # Simulate dataset with 1000 individuals and 35 chemicals to approx Mothers and Newborns cohort data.
 
 #####################
-## M&N Data #########
+## MN Data #########
 #####################
 
 # 8 phenols, 9 phthalates, 10 pbdes, 8 pcbs
+# phenols and phthalates are specific gravity adjusted
+# all values < LOD are replaced by participant-specific LOD/sqrt(2)
 
 mn_data <- mn_edc %>% dplyr::select(1:52) %>%
   select_if(~n_distinct(.) > 1) %>% #drop column if all values are the same
@@ -53,6 +55,7 @@ original <- ggplot(data = labelled, aes(x = Var1, y = Var2)) +
         strip.text.x = element_text(size = 18),
         strip.text.y = element_text(size = 18)) +
   facet_grid(Class2 ~ Class1, scales = "free", switch = "both")
+original
 
 mn_data %>% 
   gather(key = variable) %>% 
@@ -62,26 +65,30 @@ mn_data %>%
 ## Multivariate log normal ###
 ##############################
 
-# Vector of means
+# Vector of means in MN cohort
 mean_log <- as_vector(map_df(log(mn_data), ~mean(., na.rm = TRUE)))
 
-# Covariance matrix
+# Covariance matrix from MN cohort
 var_log <- cov(log(mn_data), use = "complete.obs")
 # missing values are handled by casewise deletion (and if there are no complete cases, that gives an error)
 
+# Simulate with multivariate log normal function
 set.seed(1988)
-simulated <- rlnorm.rplus(1000, meanlog = mean_log, varlog = var_log)
+simulated <- rlnorm.rplus(1000, meanlog = mean_log, varlog = var_log) %>% as.matrix() %>% as_tibble()
 colnames(simulated) <- names(mn_data)
 
+# Simulate with multivariate normal function exponentiated
 set.seed(1988)
-simulated2 <- exp(mvrnorm(n = 1000, mu = mean_log, Sigma = var_log))
+simulated2 <- exp(mvrnorm(n = 1000, mu = mean_log, Sigma = var_log)) %>% as.matrix() %>% as_tibble()
 colnames(simulated2) <- names(mn_data)
 
-simulated[1:5,1:7]
-simulated2[1:5,1:7]
+simulated
+simulated2
+
+write_csv(simulated, "./sim_data.csv")
 
 #####################
-## Heat map #########
+## Heat maps ########
 #####################
 
 sim_cormat <- round(cor(simulated, use = "pairwise.complete.obs", method = c("spearman")),2)
@@ -114,6 +121,9 @@ sim1_plot <- ggplot(data = sim_labelled, aes(x = Var1, y = Var2)) +
         strip.text.x = element_text(size = 18),
         strip.text.y = element_text(size = 18)) +
   facet_grid(Class2 ~ Class1, scales = "free", switch = "both")
+sim1_plot
+
+#####################
 
 sim2_cormat <- round(cor(simulated2, use = "pairwise.complete.obs", method = c("spearman")),2)
 
@@ -145,5 +155,8 @@ sim2_plot <- ggplot(data = sim_labelled, aes(x = Var1, y = Var2)) +
         strip.text.x = element_text(size = 18),
         strip.text.y = element_text(size = 18)) +
   facet_grid(Class2 ~ Class1, scales = "free", switch = "both")
+sim2_plot
 
-grid.arrange(original, sim1_plot, sim2_plot, nrow = 1)
+grid.arrange(original, sim1_plot, nrow = 1)
+
+grid.arrange(original, sim2_plot, nrow = 1)
