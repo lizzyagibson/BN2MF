55438a587237b447c86d8edee4bfbbaf5c521700
lizzyagibson <eag2186@cumc.columbia.edu>
2019-06-25 15:04:32 -0400

    Add 'esp' definition from MATLAB.

--- function_nmf_ee.R
+++ function_nmf_ee.R
@@ -49,33 +49,47 @@
 
 X <- chem_n
 K <- 5
+num_iter <- 10
 
-nmf_ee <- function(X, K, penalty, num_iter)
+nmf_ee <- function(X, K, penalty, num_iter) {
 
 D <- nrow(X)
 N <- ncol(X)
 
 # Initiate W and H matrices with random non-zero values
-W <- matrix(rep(1, D*K), nrow = D, ncol = K) + matrix(runif(D*K), nrow = D, ncol = K)
-H <- matrix(rep(1, K*N), nrow = K, ncol = N) + matrix(runif(K*N), nrow = K, ncol = N)
+W <- matrix(1, nrow = D, ncol = K) + matrix(runif(D*K), nrow = D, ncol = K)
+H <- matrix(1, nrow = K, ncol = N) + matrix(runif(K*N), nrow = K, ncol = N)
 
 temp <- sum(X)/sum(W %*% H)
 W <- W*sqrt(temp)
 H <- H*sqrt(temp)
 
+L <- matrix()
 
-L = [];
-for iter = 1:num_iter
-if strcmp(penalty,'L2')
-H = H.*(W'*X)./((W'*W)*H + eps);
-W = W.*(X*H')./(W*(H*H') + eps);
-# Loss function -- Gaussian MLE
-L(iter) = sum(sum((X - W*H).^2));
-elseif strcmp(penalty,'divergence')
-H = H.*((W./repmat(sum(W,1),D,1))'*(X./(W*H + eps)));
-        W = W.*((X./(W*H + eps))*(H./repmat(sum(H,2),1,N))');
-# Loss function -- Poisson MLE
-L(iter) = -sum(sum(X.*log(W*H))) + sum(sum(W*H));
-else
-  disp('Invalid penalty input');
-break;
+# Define 'eps'
+# eps returns the distance from 1.0 to the next larger double-precision number, that is, 2-52.
+eps <- 2.2204e-16
+
+# Iterate to minimize loss function
+for (i in 1:num_iter) {
+
+  if (identical(penalty,'L2')) {
+        H <- H * (t(W) %*% X) / ((t(W) %*% W) %*% H + eps)
+        W <- W * (X %*% t(H)) / (W %*% (H %*% t(H)) + eps)
+        
+  # Loss function -- Gaussian MLE
+  L(i) <- sum((X - W %*% H)^2)
+  }
+
+  else if (identical(penalty,'divergence')) {
+        H <- H * (t(W/kronecker(matrix(1,D,1), matrix(colSums(W), nrow=1))) %*% (X / (W %*% H + eps)))
+        W <- W * ((X / (W %*% H + eps)) %*% t(H / kronecker(matrix(1,1,N), matrix(rowSums(H), ncol = 1))))
+
+  # Loss function -- Poisson MLE
+  L(i) <- -sum(X * log(W %*% H)) + sum(W %*% H)
+  }
+  
+  else {print('Invalid penalty input')}
+
+  }
+}
