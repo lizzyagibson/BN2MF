0ef53a5c1e33a142f4cb5fa8c88f4a0428faee6b
lizzyagibson <eag2186@cumc.columbia.edu>
2019-06-25 15:23:38 -0400

    Return W, H, L. Fix indexing.

--- function_nmf_ee.R
+++ function_nmf_ee.R
@@ -3,9 +3,6 @@
 ## 6/25/2019 #####
 ##################
 
-# MATLAB
-
-# function [W,H,L] = NMF(X,K,penalty,num_iter)
 # % NMF implements non-negative matrix factorization of the matrix X
 # % according to a predefined penalty.
 # %
@@ -13,43 +10,6 @@
 # % K : rank of factorization, takes value in {1,2,3,...,min(D,N)}
 # % penalty : string taking one of two values, 'L2' or 'divergence'
 # % num_iter : number of iterations
-# 
-# [D,N] = size(X);
-# 
-# W = 1 + rand(D,K);
-# H = 1 + rand(K,N);
-# 
-# temp = sum(X(:))/sum(sum(W*H));
-# W = W*sqrt(temp);
-# H = H*sqrt(temp);
-# 
-# L = [];
-# for iter = 1:num_iter
-# if strcmp(penalty,'L2')
-# H = H.*(W'*X)./((W'*W)*H + eps);
-# W = W.*(X*H')./(W*(H*H') + eps);
-# L(iter) = sum(sum((X - W*H).^2));
-# elseif strcmp(penalty,'divergence')
-# H = H.*((W./repmat(sum(W,1),D,1))'*(X./(W*H + eps)));
-#         W = W.*((X./(W*H + eps))*(H./repmat(sum(H,2),1,N))');
-# L(iter) = -sum(sum(X.*log(W*H))) + sum(sum(W*H));
-# else
-#   disp('Invalid penalty input');
-# break;
-# end
-# end
-
-# % NMF implements non-negative matrix factorization of the matrix X
-# % according to a predefined penalty.
-# %
-# % X : D x N matrix of nonnegative numbers
-# % K : rank of factorization, takes value in {1,2,3,...,min(D,N)}
-# % penalty : string taking one of two values, 'L2' or 'divergence'
-# % num_iter : number of iterations
-
-X <- chem_n
-K <- 5
-num_iter <- 10
 
 nmf_ee <- function(X, K, penalty, num_iter) {
 
@@ -78,7 +38,7 @@ for (i in 1:num_iter) {
         W <- W * (X %*% t(H)) / (W %*% (H %*% t(H)) + eps)
         
   # Loss function -- Gaussian MLE
-  L(i) <- sum((X - W %*% H)^2)
+  L[i] <- sum((X - W %*% H)^2)
   }
 
   else if (identical(penalty,'divergence')) {
@@ -86,10 +46,15 @@ for (i in 1:num_iter) {
         W <- W * ((X / (W %*% H + eps)) %*% t(H / kronecker(matrix(1,1,N), matrix(rowSums(H), ncol = 1))))
 
   # Loss function -- Poisson MLE
-  L(i) <- -sum(X * log(W %*% H)) + sum(W %*% H)
+  L[i] <- -sum(X * log(W %*% H)) + sum(W %*% H)
   }
+  # L(iter) = -sum(sum(X.*log(W*H))) + sum(sum(W*H));
   
   else {print('Invalid penalty input')}
-
   }
+
+return(list(ind_scores = W, chem_loadings = H, loss = L))
+
 }
+
+nmf_ee(chem_n, 5, "divergence", 10)
