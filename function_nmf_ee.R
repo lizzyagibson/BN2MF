918b1ac35465ac9082e49f044732d5b0cf0e3a7d
lizzyagibson <eag2186@cumc.columbia.edu>
2019-06-25 15:57:43 -0400

    Edit if else criteria.

--- function_nmf_ee.R
+++ function_nmf_ee.R
@@ -5,7 +5,7 @@
 
 # % NMF implements non-negative matrix factorization of the matrix X
 # % according to a predefined penalty.
-# %
+
 # % X : D x N matrix of nonnegative numbers
 # % K : rank of factorization, takes value in {1,2,3,...,min(D,N)}
 # % penalty : string taking one of two values, 'L2' or 'divergence'
@@ -31,7 +31,7 @@ L <- matrix()
 eps <- 2.2204e-16
 
 # Iterate to minimize loss function
-if (identical(penalty,'L2')) {
+if (penalty == 'L2') {
   for (i in 1:num_iter) {
         H <- H * (t(W) %*% X) / ((t(W) %*% W) %*% H + eps)
         W <- W * (X %*% t(H)) / (W %*% (H %*% t(H)) + eps)
@@ -42,7 +42,7 @@ if (identical(penalty,'L2')) {
   return(list(ind_scores = W, chem_loadings = H, loss = L))
   }
 
-  else if (identical(penalty,'divergence')) {
+  else if (penalty == 'divergence') {
     for (i in 1:num_iter) {
         H <- H * (t(W/kronecker(matrix(1,D,1), matrix(colSums(W), nrow=1))) %*% (X / (W %*% H + eps)))
         W <- W * ((X / (W %*% H + eps)) %*% t(H / kronecker(matrix(1,1,N), matrix(rowSums(H), ncol = 1))))
@@ -53,8 +53,5 @@ if (identical(penalty,'L2')) {
   return(list(ind_scores = W, chem_loadings = H, loss = L))
   }
 
-  else {print('Invalid penalty input')}
+  else {print('Invalid penalty input.')}
 }
-
-
-nmf_ee(chem_n, 5, "divergence", 10)
