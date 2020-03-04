582d94470056bdb47c4df7faee574359b7bf1566
lizzyagibson <eag2186@cumc.columbia.edu>
2020-03-04 11:20:43 -0500

    Inspect GNMF relative error.

--- MATLAB/nmf_sim3.m
+++ MATLAB/nmf_sim3.m
@@ -1,6 +1,6 @@
 %% Make this whole thing loop
 
-%for i = 1:100
+for i = 1:100
     
 %% Setup the Import Options and import the data
 opts = delimitedTextImportOptions("NumVariables", 50);
@@ -18,8 +18,8 @@ opts.ExtraColumnsRule = "ignore";
 opts.EmptyLineRule = "read";
 
 % Import the data
-%simdata3 = readtable(strcat("/Users/lizzy/OneDrive/Columbia/Spring 2020/nmf/Sims/Iterate/sim_cor_", num2str(i), ".csv"), opts);
-simdata3 = readtable("/Users/lizzy/OneDrive/Columbia/Spring 2020/nmf/Sims/Iterate/sim_cor_1.csv", opts);
+simdata3 = readtable(strcat("/Users/lizzy/OneDrive/Columbia/Spring 2020/nmf/Sims/Iterate/sim_cor_", num2str(i), ".csv"), opts);
+%simdata3 = readtable("/Users/lizzy/OneDrive/Columbia/Spring 2020/nmf/Sims/Iterate/sim_cor_1.csv", opts);
 
 %% Convert to output type
 simdata3 = table2array(simdata3);
@@ -27,7 +27,7 @@ simdata3 = table2array(simdata3);
 %% Clear temporary variables
 clear opts
 
-[ewa3,eh3, a3, e_score, score] = NPBayesNMF(simdata3, 50, 1000);
+[ewa3, eh3, a3] = NPBayesNMF(simdata3, 50, 10000);
 
 %labels = ["pcb28", "pcb66", "pcb74", "pcb99", "pcb105", "pcb118", "pcb138_158" "pcb146", "pcb153", "pcb156", "pcb167", "pcb170", "pcb178", "pcb183", "pcb187", "pcb180", "pcb189", "pcb194", "pcb196_203" "pcb199", "pcb206", "pcb209", "BDE17", "BDE28", "BDE47", "BDE66", "BDE85", "BDE99", "BDE100", "BDE153", "BDE154", "BDE183", "BDE209", "MECPP", "MEHHP", "MEOHP", "MCPP", "MIBP", "MBP", "MBZP", "MEP", "MEHP", "dcp_24", "dcp_25", "b_pb", "bp_3", "m_pb", "p_pb", "tcs", "bpa"];
 
@@ -50,8 +50,8 @@ clear opts
 % set(gca,'XTick',1:size(eh3,2));
 % set(gca,'XTickLabels',labels);
 
-%save(strcat("/Users/lizzy/OneDrive/Columbia/Spring 2020/nmf/Sims/Iterate_Out/ewa_cor_", num2str(i), ".mat"), 'ewa3');
-%save(strcat("/Users/lizzy/OneDrive/Columbia/Spring 2020/nmf/Sims/Iterate_Out/eh_cor_", num2str(i), ".mat"), 'eh3');
+save(strcat("/Users/lizzy/OneDrive/Columbia/Spring 2020/nmf/Sims/Iterate_Out/ewa_cor_", num2str(i), ".mat"), 'ewa3');
+save(strcat("/Users/lizzy/OneDrive/Columbia/Spring 2020/nmf/Sims/Iterate_Out/eh_cor_", num2str(i), ".mat"), 'eh3');
 
-%end
+end
 
