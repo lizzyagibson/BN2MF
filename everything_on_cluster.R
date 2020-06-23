3aa44342d9e1ea55cab558ecbb8036fd929d98d3
lizzyagibson <eag2186@cumc.columbia.edu>
2020-06-23 11:27:12 -0400

    Write sh to run on HPC.

--- everything_on_cluster.R
+++ everything_on_cluster.R
@@ -6,17 +6,21 @@
 
 # Packages
 library(tidyverse)
-library(NMF)
-library(psych) # Factor Analysis
+library(registry, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+library(pkgmaker, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+library(rngtools, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+library(NMF, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+library(psych, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
 
 # Read in Sims
-load(here::here("./R/Sims/Iterate/sim_dist.RDA"))
-load(here::here("./R/Sims/Iterate/sim_cor.RDA"))
-load(here::here("./R/Sims/Iterate/sim_over.RDA"))
+load("Data/sim_dist.RDA")
+load("Data/sim_cor.RDA")
+load("Data/sim_over.RDA")
 
-# sim_dist <- sim_dist %>% rename(true_patterns = patterns, true_scores = scores) %>% select(-chem, -noise) %>% slice(1:2)
-# sim_over <- sim_over %>% rename(true_patterns = patterns, true_scores = scores) %>% select(-chem, -noise) %>% slice(1:2)
-# sim_cor  <- sim_cor %>% rename(true_patterns = patterns, true_scores = scores) %>% select(-chem, -noise) %>% slice(1:2)
+## read job number from system environment
+## This only works if run on cluster!!
+job_num = as.integer(Sys.getenv("SGE_TASK_ID"))
+job_num 
 
 # Run everything
 
@@ -51,7 +55,7 @@ get_pca <- function (sim) {
 
 ## Run
 
-out_dist <- sim_dist %>% 
+out_dist <- sim_dist[job_num,] %>% 
   mutate(pca_out = map(sim, get_pca),
          pca_rotations = map(pca_out, function(x)
                           x[[1]]),
@@ -62,7 +66,7 @@ out_dist <- sim_dist %>%
          pca_rank      = map(pca_out, function(x)
                           x[[4]]))
 
-out_over <- sim_over %>% 
+out_over <- sim_over[job_num,] %>% 
   mutate(pca_out = map(sim, get_pca),
          pca_rotations = map(pca_out, function(x)
            x[[1]]),
@@ -73,7 +77,7 @@ out_over <- sim_over %>%
          pca_rank      = map(pca_out, function(x)
            x[[4]]))
 
-out_cor <- sim_cor %>% 
+out_cor <- sim_cor[job_num,] %>% 
   mutate(pca_out = map(sim, get_pca),
          pca_rotations = map(pca_out, function(x)
            x[[1]]),
@@ -288,200 +292,11 @@ out_cor <- out_cor %>%
 # Code for Matching #
 #####################
 
-# Symmetric Subspace Distance
-
-## Function
-
-symm_subspace_dist <- function(U, V) {
-  
-  if (nrow(U) != nrow(V)) stop("Matrices must have same number of participants (rows).")
-  
-  qrU <- qr.Q(qr(U))
-  qrV <- qr.Q(qr(V))
-  
-  m <- ncol(U)
-  n <- ncol(V)
-  
-  dUV <- sqrt( max(m,n) - sum((t(qrU) %*% qrV)^2) )
-  
-  ratio <- dUV/sqrt( max(m,n))
-  
-  list(symm_subspace_dist = dUV, Similarity = ratio)
-  
-}
-
-## Run
-
-### PCA
-
-out_dist <- out_dist %>% 
-  mutate(pca_score_ssd = map2(true_scores, pca_scores, symm_subspace_dist),
-         pca_score_ssdist = map(pca_score_ssd, function(x)
-           x[[1]]),
-         pca_score_sssimilarity = map(pca_score_ssd, function(x)
-           x[[2]]),
-         pca_loading_ssd = map2(true_patterns, pca_rotations, function(x,y) symm_subspace_dist(t(x), y)),
-         pca_loading_ssdist = map(pca_loading_ssd, function(x)
-           x[[1]]),
-         pca_loading_sssimilarity = map(pca_loading_ssd, function(x)
-           x[[2]]))
-
-out_over <- out_over %>% 
-  mutate(pca_score_ssd = map2(true_scores, pca_scores, symm_subspace_dist),
-         pca_score_ssdist = map(pca_score_ssd, function(x)
-           x[[1]]),
-         pca_score_sssimilarity = map(pca_score_ssd, function(x)
-           x[[2]]),
-         pca_loading_ssd = map2(true_patterns, pca_rotations, function(x,y) symm_subspace_dist(t(x), y)),
-         pca_loading_ssdist = map(pca_loading_ssd, function(x)
-           x[[1]]),
-         pca_loading_sssimilarity = map(pca_loading_ssd, function(x)
-           x[[2]]))
-
-out_cor <- out_cor %>% 
-  mutate(pca_score_ssd = map2(true_scores, pca_scores, symm_subspace_dist),
-         pca_score_ssdist = map(pca_score_ssd, function(x)
-           x[[1]]),
-         pca_score_sssimilarity = map(pca_score_ssd, function(x)
-           x[[2]]),
-         pca_loading_ssd = map2(true_patterns, pca_rotations, function(x,y) symm_subspace_dist(t(x), y)),
-         pca_loading_ssdist = map(pca_loading_ssd, function(x)
-           x[[1]]),
-         pca_loading_sssimilarity = map(pca_loading_ssd, function(x)
-           x[[2]]))
-
-### FA
-
-out_dist <- out_dist %>% 
-  mutate(fa_score_ssd = map2(true_scores, fa_scores, symm_subspace_dist),
-         fa_score_ssdist = map(fa_score_ssd, function(x)
-           x[[1]]),
-         fa_score_sssimilarity = map(fa_score_ssd, function(x)
-           x[[2]]),
-         fa_loading_ssd = map2(true_patterns, fa_rotations, function(x,y) symm_subspace_dist(t(x), y)),
-         fa_loading_ssdist = map(fa_loading_ssd, function(x)
-           x[[1]]),
-         fa_loading_sssimilarity = map(fa_loading_ssd, function(x)
-           x[[2]]))
-
-out_over <- out_over %>% 
-  mutate(fa_score_ssd = map2(true_scores, fa_scores, symm_subspace_dist),
-         fa_score_ssdist = map(fa_score_ssd, function(x)
-           x[[1]]),
-         fa_score_sssimilarity = map(fa_score_ssd, function(x)
-           x[[2]]),
-         fa_loading_ssd = map2(true_patterns, fa_rotations, function(x,y) symm_subspace_dist(t(x), y)),
-         fa_loading_ssdist = map(fa_loading_ssd, function(x)
-           x[[1]]),
-         fa_loading_sssimilarity = map(fa_loading_ssd, function(x)
-           x[[2]]))
-
-out_cor <- out_cor %>% 
-  mutate(fa_score_ssd = map2(true_scores, fa_scores, symm_subspace_dist),
-         fa_score_ssdist = map(fa_score_ssd, function(x)
-           x[[1]]),
-         fa_score_sssimilarity = map(fa_score_ssd, function(x)
-           x[[2]]),
-         fa_loading_ssd = map2(true_patterns, fa_rotations, function(x,y) symm_subspace_dist(t(x), y)),
-         fa_loading_ssdist = map(fa_loading_ssd, function(x)
-           x[[1]]),
-         fa_loading_sssimilarity = map(fa_loading_ssd, function(x)
-           x[[2]]))
-
-### NMF L2
-
-out_dist <- out_dist %>% 
-  mutate(nmf_l2_score_ssd = map2(true_scores, nmf_l2_scores, symm_subspace_dist),
-         nmf_l2_score_ssdist = map(nmf_l2_score_ssd, function(x)
-           x[[1]]),
-         nmf_l2_score_sssimilarity = map(nmf_l2_score_ssd, function(x)
-           x[[2]]),
-         nmf_l2_loading_ssd = map2(true_patterns, nmf_l2_loadings, function(x,y) symm_subspace_dist(t(x), t(y))),
-         nmf_l2_loading_ssdist = map(nmf_l2_loading_ssd, function(x)
-           x[[1]]),
-         nmf_l2_loading_sssimilarity = map(nmf_l2_loading_ssd, function(x)
-           x[[2]]))
-
-out_over <- out_over %>% 
-  mutate(nmf_l2_score_ssd = map2(true_scores, nmf_l2_scores, symm_subspace_dist),
-         nmf_l2_score_ssdist = map(nmf_l2_score_ssd, function(x)
-           x[[1]]),
-         nmf_l2_score_sssimilarity = map(nmf_l2_score_ssd, function(x)
-           x[[2]]),
-         nmf_l2_loading_ssd = map2(true_patterns, nmf_l2_loadings, function(x,y) symm_subspace_dist(t(x), t(y))),
-         nmf_l2_loading_ssdist = map(nmf_l2_loading_ssd, function(x)
-           x[[1]]),
-         nmf_l2_loading_sssimilarity = map(nmf_l2_loading_ssd, function(x)
-           x[[2]]))
-
-out_cor <- out_cor %>% 
-  mutate(nmf_l2_score_ssd = map2(true_scores, nmf_l2_scores, symm_subspace_dist),
-         nmf_l2_score_ssdist = map(nmf_l2_score_ssd, function(x)
-           x[[1]]),
-         nmf_l2_score_sssimilarity = map(nmf_l2_score_ssd, function(x)
-           x[[2]]),
-         nmf_l2_loading_ssd = map2(true_patterns, nmf_l2_loadings, function(x,y) symm_subspace_dist(t(x), t(y))),
-         nmf_l2_loading_ssdist = map(nmf_l2_loading_ssd, function(x)
-           x[[1]]),
-         nmf_l2_loading_sssimilarity = map(nmf_l2_loading_ssd, function(x)
-           x[[2]]))
-
-### NMF Poisson
-
-out_dist <- out_dist %>% 
-              mutate(nmf_p_score_ssd = map2(true_scores, nmf_p_scores, symm_subspace_dist),
-            nmf_p_score_ssdist = map(nmf_p_score_ssd, function(x)
-              x[[1]]),
-            nmf_p_score_sssimilarity = map(nmf_p_score_ssd, function(x)
-              x[[2]]),
-            nmf_p_loading_ssd = map2(true_patterns, nmf_p_loadings, function(x,y) symm_subspace_dist(t(x), t(y))),
-            nmf_p_loading_ssdist = map(nmf_p_loading_ssd, function(x)
-              x[[1]]),
-            nmf_p_loading_sssimilarity = map(nmf_p_loading_ssd, function(x)
-              x[[2]]))
-
-out_over <- out_over %>% 
-  mutate(nmf_p_score_ssd = map2(true_scores, nmf_p_scores, symm_subspace_dist),
-         nmf_p_score_ssdist = map(nmf_p_score_ssd, function(x)
-           x[[1]]),
-         nmf_p_score_sssimilarity = map(nmf_p_score_ssd, function(x)
-           x[[2]]),
-         nmf_p_loading_ssd = map2(true_patterns, nmf_p_loadings, function(x,y) symm_subspace_dist(t(x), t(y))),
-         nmf_p_loading_ssdist = map(nmf_p_loading_ssd, function(x)
-           x[[1]]),
-         nmf_p_loading_sssimilarity = map(nmf_p_loading_ssd, function(x)
-           x[[2]]))
-
-out_cor <- out_cor %>% 
-  mutate(nmf_p_score_ssd = map2(true_scores, nmf_p_scores, symm_subspace_dist),
-         nmf_p_score_ssdist = map(nmf_p_score_ssd, function(x)
-           x[[1]]),
-         nmf_p_score_sssimilarity = map(nmf_p_score_ssd, function(x)
-           x[[2]]),
-         nmf_p_loading_ssd = map2(true_patterns, nmf_p_loadings, function(x,y) symm_subspace_dist(t(x), t(y))),
-         nmf_p_loading_ssdist = map(nmf_p_loading_ssd, function(x)
-           x[[1]]),
-         nmf_p_loading_sssimilarity = map(nmf_p_loading_ssd, function(x)
-           x[[2]]))
+out_dist <- out_dist %>% dplyr::select(-pca_out, -fa_out, -nmf_l2_out, -nmf_p_out)
+out_over <- out_over %>% dplyr::select(-pca_out, -fa_out, -nmf_l2_out, -nmf_p_out)
+out_cor <- out_cor %>% dplyr::select(-pca_out, -fa_out, -nmf_l2_out, -nmf_p_out)
 
 save(out_dist, file = paste0("out_", job_num, "_dist.RDA"))
 save(out_over, file = paste0("out_", job_num, "_over.RDA"))
 save(out_cor, file = paste0("out_", job_num, "_cor.RDA"))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-           
-           
-           
