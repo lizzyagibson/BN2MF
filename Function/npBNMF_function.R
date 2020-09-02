f7cea07222d6dafaa1ba82b41803dba56fe402db
lizzyagibson <eag2186@cumc.columbia.edu>
2020-09-02 11:54:49 -0400

    Bug fixes in npBNMF R code.

--- Function/npBNMF_function.R
+++ Function/npBNMF_function.R
@@ -6,7 +6,8 @@
 # %           values of these matrices according to their approximate posterior variational distributions.
 # % num_iter is the number of iterations to run. The code terminates based on convergence currently.
 
-X = matrix(runif(50), 10, 5)
+# X = matrix(runif(50), 10, 5)
+# X = edc_cc
 
 NPBayesNMF <- function(X) {
 
@@ -29,8 +30,8 @@ NPBayesNMF <- function(X) {
     
       w01 = 1
       w02 = 1
-  
-      W1 = apply(dim*matrix(1, nrow = dim, ncol = Kinit), 2, function(x) rgamma(x, 1/dim))
+    
+      W1 = matrix(rgamma(dim*Kinit, shape = dim, scale = 1/dim), nrow = dim, ncol = Kinit)
       W2 = dim*matrix(1, nrow = dim, ncol = Kinit)
       
       a01 = bnp_switch*1/Kinit + (1-bnp_switch)
@@ -56,21 +57,20 @@ NPBayesNMF <- function(X) {
           
           # The equivalent of Matlab's repmat(a,2,3) in base R is kronecker(matrix(1,2,3),a)
           X_reshape = kronecker(array(1, dim = c(K,1,1)), array(t(X), c(1, N, dim)))
-          ElnWA = digamma(W1) - log(W2) + kronecker(array(1, dim), digamma(A1)-log(A2))
+          ElnWA = digamma(W1) - log(W2) + kronecker(array(1, c(dim,1)), digamma(A1)-log(A2))
           ElnWA_reshape = kronecker(array(1, dim = c(1,N,1)), array(t(ElnWA), c(K, 1, dim)))
-            
-          t1 = array(apply(ElnWA_reshape,c(2,3), max), c(1, 5, 10))
-          
+
+          t1 = array(apply(ElnWA_reshape,c(2,3), max), c(1, Kinit, dim))
+
           # % Expected value of log W * expected value of log A
           ElnWA_reshape = ElnWA_reshape - kronecker(array(1, dim = c(K,1,1)), t1)
-          
           # % expected value of log H
           ElnH = digamma(H1) - log(H2)                               
-                                         
+          
           P = ElnWA_reshape + kronecker(array(1, dim=c(1,1,dim)), ElnH)
           P = exp(P)
-          P = P / kronecker(array(1, dim = c(K,1,1)), array(apply(P, 3, colSums), dim = c(1, K, dim)))
-        
+          P = P / kronecker(array(1, dim = c(K,1,1)), array(apply(P, 3, colSums), dim = c(1, Kinit, dim)))
+
           # % P is a probability to put a lower bound on the ELBO
           # % expected value of log WAH is concave
           # % include P to make expectation tractable
@@ -92,24 +92,24 @@ NPBayesNMF <- function(X) {
           
           if (length(idx_prune) >= 1) {
                   W1 = W1[,-idx_prune]
-                  W2 = We[,-idx_prune]
-                  A1 = A1[,-idx_prune]
-                  A2 = A2[,-idx_prune]
-                  H1 = H1[,-idx_prune]
-                  H2 = H2[,-idx_prune] 
+                  W2 = W2[,-idx_prune]
+                  A1 = matrix(A1[,-idx_prune], nrow = 1)
+                  A2 = matrix(A2[,-idx_prune], nrow = 1)
+                  H1 = H1[-idx_prune,]
+                  H2 = H2[-idx_prune,] 
                   }
           
           K = length(A1)
         
           score[iter] = base::sum(abs(X- (W1/W2) %*% diag(as.vector(A1/A2)) %*% (H1/H2)))
           
-          if (iter %% 100 == 0) {print(paste0("Run Number: ", i, "; Iter Number: ", iter, "; Iter Score: ", round(score[iter], 5)))}
+          if (iter %% 100 == 0) {print(paste0("Run Number: ", i, "; Iter Number: ", iter, "; Iter Score: ", round(score[iter], 4)))}
           if (iter > 1 && abs(score[iter-1] - score[iter]) < 1e-5) {break} # Convergence criteria!
           }
 
       end_score[i] = score[tail(which(score != 0),1)]
           
-      print(paste0("Run Number: ", i, "; Final Score: ", round(end_score[i], 5)))
+      print(paste0("Run Number: ", i, "; Final Score: ", round(end_score[i], 4)))
       
       # % Among the results, use the fitted variational parameters that achieve the highest ELBO
       if (i == 1 | (i > 1 && (end_score[i] >= max(end_score)))) {
@@ -128,4 +128,4 @@ NPBayesNMF <- function(X) {
   list(EWA = EWA, EH = EH, H_CI_low = H_CI_low, H_CI_high = H_CI_high)
   }
 
-NPBayesNMF(X)
+# NPBayesNMF(X)
