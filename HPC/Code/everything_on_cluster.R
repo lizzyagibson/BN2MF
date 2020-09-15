03151c48592b9f36f1299bb7ec80228cc39ec119
lizzyagibson <eag2186@cumc.columbia.edu>
2020-09-15 14:11:31 -0400

    Add BNMF to HPC script.

--- HPC/Code/everything_on_cluster.R
+++ HPC/Code/everything_on_cluster.R
@@ -13,8 +13,138 @@ library(NMF, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
 library(GPArotation, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
 library(psych, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
 
+##### Read function
+NPBayesNMF <- function(X) {
+  X = as.matrix(X)
+  bnp_switch = 1
+  dim = nrow(X)
+  N = ncol(X)
+  Kinit = ncol(X)
+  
+  nruns = 100
+  end_score = matrix(rep(0, times = nruns))
+  
+  EA = matrix()
+  EWA = matrix()
+  EH = matrix()
+  EW = matrix()
+  
+  for (i in 1:nruns) {
+    
+    K = Kinit
+    
+    w01 = 1
+    w02 = 1
+    
+    W1 = matrix(rgamma(dim*Kinit, shape = dim, scale = 1/dim), nrow = dim, ncol = Kinit)
+    W2 = dim*matrix(1, nrow = dim, ncol = Kinit)
+    
+    a01 = bnp_switch*1/Kinit + (1-bnp_switch)
+    a02 = 1
+    
+    A1 = a01 + bnp_switch*1000*matrix(1, 1, Kinit)/Kinit
+    A2 = a02 + bnp_switch*1000*matrix(1, 1, Kinit)
+    
+    h01 = 1 # Non-sparse prior
+    # h01 = 1/Kinit # Sparse prior
+    h02 = 1
+    
+    H1 = matrix(1, Kinit, N)
+    H2 = matrix(1, Kinit, N)
+    
+    num_iter = 100000
+    
+    score = vector("numeric", length = num_iter)
+    
+    for (iter in 1:num_iter) {
+      
+      EW = W1 / W2
+      
+      # The equivalent of Matlab's repmat(a,2,3) in base R is kronecker(matrix(1,2,3),a)
+      X_reshape = kronecker(array(1, dim = c(K,1,1)), array(t(X), c(1, N, dim)))
+      ElnWA = digamma(W1) - log(W2) + kronecker(array(1, c(dim,1)), digamma(A1)-log(A2))
+      ElnWA_reshape = kronecker(array(1, dim = c(1,N,1)), array(t(ElnWA), c(K, 1, dim)))
+      
+      t1 = array(apply(ElnWA_reshape,c(2,3), max), c(1, Kinit, dim))
+      
+      # % Expected value of log W * expected value of log A
+      ElnWA_reshape = ElnWA_reshape - kronecker(array(1, dim = c(K,1,1)), t1)
+      # % expected value of log H
+      ElnH = digamma(H1) - log(H2)                               
+      
+      P = ElnWA_reshape + kronecker(array(1, dim=c(1,1,dim)), ElnH)
+      P = exp(P)
+      P = P / kronecker(array(1, dim = c(K,1,1)), array(apply(P, 3, colSums), dim = c(1, Kinit, dim)))
+      
+      # % P is a probability to put a lower bound on the ELBO
+      # % expected value of log WAH is concave
+      # % include P to make expectation tractable
+      # % These are update steps from optimizing the ELBO 
+      # % take the gradient with respect to parameter, set to zero, solve
+      
+      H1 = h01 + apply(P * X_reshape, c(1,2), sum)
+      H2 = h02 + t(kronecker(matrix(1, nrow = N, ncol = 1), 
+                             matrix(colSums(EW * kronecker(matrix(1, nrow = dim, ncol = 1), A1/A2)), nrow=1)))
+      
+      W1 = w01 + t(array(apply(X_reshape*P, c(1,3), sum), dim = c(K, dim)))
+      W2 = w02 + kronecker(matrix(1, dim, 1), t(rowSums((H1/H2) * kronecker(matrix(1, 1, N), t(A1/A2)))))
+      
+      A1 = a01 + bnp_switch * t(rowSums(apply(X_reshape*P, c(1,2), sum)))
+      A2 = a02 + bnp_switch * (colSums(W1/W2) * t(rowSums(H1/H2)))
+      
+      # % This is the sparse prior on A, pushing A to zero
+      # If all patterns are zero, don't push anything
+      # prohibits pushing all patterns to zero
+      if ( all(A1/A2 < 10^-3) ) {idx_prune = integer(0)} else {idx_prune = which(A1/A2 < 10^-3)} 
+      
+      if (length(idx_prune) >= 1) {
+        W1 = matrix(W1[,-idx_prune], nrow = dim)
+        W2 = matrix(W2[,-idx_prune], nrow = dim)
+        A1 = matrix(A1[,-idx_prune], nrow = 1)
+        A2 = matrix(A2[,-idx_prune], nrow = 1)
+        H1 = matrix(H1[-idx_prune,], ncol = N)
+        H2 = matrix(H2[-idx_prune,], ncol = N)
+      }
+      
+      K = length(A1)
+      
+      score[iter] = if (ncol(A1/A2) > 1) {
+        sum( abs( X - (W1/W2) %*% diag(as.vector(A1/A2)) %*% (H1/H2) ) )
+      } else if (ncol(A1/A2) == 1) {
+        sum( abs( X - (W1/W2) %*% as.vector(A1/A2) %*% (H1/H2) ) )
+      }
+      
+      if (iter %% 100 == 0) {print(paste0("Run Number: ", i, "; Iter Number: ", iter, "; Iter Score: ", round(score[iter], 4)))}
+      if (iter > 1 && abs(score[iter-1] - score[iter]) < 1e-5) {
+        print(paste0('Converged in ', iter,' iterations.'))
+        break} # Convergence criteria!
+    }
+    
+    if( iter == num_iter ) warning('Maximum iterations reached. BNMF did not converge.')
+    end_score[i] = score[tail(which(score != 0),1)]
+    
+    #print(paste0("Run Number: ", i, "; Iter Number: ", iter, "; Final Score: ", round(end_score[i], 4)))
+    
+    # % Among the results, use the fitted variational parameters that achieve the highest ELBO
+    if (i == 1 | (i > 1 && (end_score[i] >= max(end_score)))) {
+      EA = A1/A2
+      EWA = (W1/W2)*diag(A1/A2)
+      EH = H1/H2
+      EW = (W1/W2)
+      varA = A1/(A2^2)
+      varW = W1/(W2^2)
+      alphaH = H1
+      betaH = H2
+    }
+    H_CI_low  <- qgamma(0.025, shape = alphaH, rate = betaH)
+    H_CI_high <- qgamma(0.975, shape = alphaH, rate = betaH)
+  }
+  
+  list(EWA = EWA, EH = EH, H_CI_low = H_CI_low, H_CI_high = H_CI_high)
+}
+
 # Read in Sims
-load("Data/sim_all.RDA")
+load("Data/sim_bnmf.RDA")
 
 ## read job number from system environment
 ## This only works if run on cluster!!
@@ -372,6 +502,34 @@ output_all <- output_all %>%
                               match_nmf_scores))
 
 
+#####
+# np BNMF
+#####
+
+get_bnmf <- function (sim) {
+  set.seed(1988)
+  bnmf_out <- NPBayesNMF(sim)
+  
+  ewa <- bnmf_out$EWA
+  eh <- bnmf_out$EH
+  pred <- ewa %*% eh
+  rank = ncol(ewa)
+  
+  list(ewa = ewa, eh = eh, pred = pred, rank = rank)
+}
+
+output_all <- output_all %>% 
+              mutate(bnmf_out         = map(sim, get_bnmf),
+                     bnmf_scores_un   = map(bnmf_out, function(x) x[[1]]),
+                     bnmf_loadings_un = map(bnmf_out, function(x) x[[2]]),
+                     bnmf_pred        = map(bnmf_out, function(x) x[[3]]),
+                     bnmf_rank        = map(bnmf_out, function(x) x[[4]]))
+            
+output_all <- output_all %>%
+  mutate(bnmf_loadings = map2(true_patterns, bnmf_loadings_un, match_nmf_patterns),
+         bnmf_scores = pmap(list(bnmf_loadings_un, bnmf_loadings, bnmf_scores_un), 
+                             match_nmf_scores))
+
 ###############################
 # Symmetric Subspace Distance #
 ###############################
@@ -399,7 +557,8 @@ output_all <- output_all %>%
   mutate(pca_norm = map2(sim, pca_pred, function(x,y) norm(x-y, "F")/norm(x, "F")),
          fa_norm = map2(sim, fa_pred, function(x,y) norm(x-y, "F")/norm(x, "F")),
          nmf_l2_norm = map2(sim, nmf_l2_pred, function(x,y) norm(x-y, "F")/norm(x, "F")),
-         nmf_P_norm = map2(sim, nmf_p_pred, function(x,y) norm(x-y, "F")/norm(x, "F")),
+         nmf_p_norm = map2(sim, nmf_p_pred, function(x,y) norm(x-y, "F")/norm(x, "F")),
+         bnmf_norm = map2(sim, bnmf_pred, function(x,y) norm(x-y, "F")/norm(x, "F")),
          pca_rotation_ssdist   = map2(true_patterns, pca_rotations, symm_subspace_dist),
          pca_scores_ssdist     = map2(true_scores, pca_scores, symm_subspace_dist),
          fa_rotations_ssdist   = map2(true_patterns, fa_rotations, symm_subspace_dist),
@@ -407,7 +566,9 @@ output_all <- output_all %>%
          nmf_l2_loading_ssdist = map2(true_patterns, nmf_l2_loadings, symm_subspace_dist),
          nmf_l2_scores_ssdist  = map2(true_scores, nmf_l2_scores, symm_subspace_dist),
          nmf_p_loading_ssdist  = map2(true_patterns, nmf_p_loadings, symm_subspace_dist),
-         nmf_p_scores_ssdist   = map2(true_scores, nmf_p_scores, symm_subspace_dist))
+         nmf_p_scores_ssdist   = map2(true_scores, nmf_p_scores, symm_subspace_dist),
+         bnmf_loading_ssdist   = map2(true_patterns, bnmf_loadings, symm_subspace_dist),
+         bnmf_scores_ssdist    = map2(true_scores, bnmf_scores, symm_subspace_dist))
 
 #####
 # Save
