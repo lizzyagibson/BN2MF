fc6bf67f2db54401f8cdff206595c7ef694c366a
lizzyagibson <eag2186@cumc.columbia.edu>
2020-09-18 14:02:43 -0400

    Adapt code to accept all output dim.

--- HPC/Code/everything_on_cluster.R
+++ HPC/Code/everything_on_cluster.R
@@ -14,136 +14,6 @@ library(NMF, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
 library(GPArotation, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
 library(psych, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
 
-##### Read function
-NPBayesNMF <- function(X) {
-  X = as.matrix(X)
-  bnp_switch = 1
-  dim = nrow(X)
-  N = ncol(X)
-  Kinit = ncol(X)
-  
-  nruns = 100
-  end_score = matrix(rep(0, times = nruns))
-  
-  EA = matrix()
-  EWA = matrix()
-  EH = matrix()
-  EW = matrix()
-  
-  for (i in 1:nruns) {
-    
-    K = Kinit
-    
-    w01 = 1
-    w02 = 1
-    
-    W1 = matrix(rgamma(dim*Kinit, shape = dim, scale = 1/dim), nrow = dim, ncol = Kinit)
-    W2 = dim*matrix(1, nrow = dim, ncol = Kinit)
-    
-    a01 = bnp_switch*1/Kinit + (1-bnp_switch)
-    a02 = 1
-    
-    A1 = a01 + bnp_switch*1000*matrix(1, 1, Kinit)/Kinit
-    A2 = a02 + bnp_switch*1000*matrix(1, 1, Kinit)
-    
-    h01 = 1 # Non-sparse prior
-    # h01 = 1/Kinit # Sparse prior
-    h02 = 1
-    
-    H1 = matrix(1, Kinit, N)
-    H2 = matrix(1, Kinit, N)
-    
-    num_iter = 100000
-    
-    score = vector("numeric", length = num_iter)
-    
-    for (iter in 1:num_iter) {
-      
-      EW = W1 / W2
-      
-      # The equivalent of Matlab's repmat(a,2,3) in base R is kronecker(matrix(1,2,3),a)
-      X_reshape = kronecker(array(1, dim = c(K,1,1)), array(t(X), c(1, N, dim)))
-      ElnWA = digamma(W1) - log(W2) + kronecker(array(1, c(dim,1)), digamma(A1)-log(A2))
-      ElnWA_reshape = kronecker(array(1, dim = c(1,N,1)), array(t(ElnWA), c(K, 1, dim)))
-      
-      t1 = array(apply(ElnWA_reshape,c(2,3), max), c(1, Kinit, dim))
-      
-      # % Expected value of log W * expected value of log A
-      ElnWA_reshape = ElnWA_reshape - kronecker(array(1, dim = c(K,1,1)), t1)
-      # % expected value of log H
-      ElnH = digamma(H1) - log(H2)                               
-      
-      P = ElnWA_reshape + kronecker(array(1, dim=c(1,1,dim)), ElnH)
-      P = exp(P)
-      P = P / kronecker(array(1, dim = c(K,1,1)), array(apply(P, 3, colSums), dim = c(1, Kinit, dim)))
-      
-      # % P is a probability to put a lower bound on the ELBO
-      # % expected value of log WAH is concave
-      # % include P to make expectation tractable
-      # % These are update steps from optimizing the ELBO 
-      # % take the gradient with respect to parameter, set to zero, solve
-      
-      H1 = h01 + apply(P * X_reshape, c(1,2), sum)
-      H2 = h02 + t(kronecker(matrix(1, nrow = N, ncol = 1), 
-                             matrix(colSums(EW * kronecker(matrix(1, nrow = dim, ncol = 1), A1/A2)), nrow=1)))
-      
-      W1 = w01 + t(array(apply(X_reshape*P, c(1,3), sum), dim = c(K, dim)))
-      W2 = w02 + kronecker(matrix(1, dim, 1), t(rowSums((H1/H2) * kronecker(matrix(1, 1, N), t(A1/A2)))))
-      
-      A1 = a01 + bnp_switch * t(rowSums(apply(X_reshape*P, c(1,2), sum)))
-      A2 = a02 + bnp_switch * (colSums(W1/W2) * t(rowSums(H1/H2)))
-      
-      # % This is the sparse prior on A, pushing A to zero
-      # If all patterns are zero, don't push anything
-      # prohibits pushing all patterns to zero
-      if ( all(A1/A2 < 10^-3) ) {idx_prune = integer(0)} else {idx_prune = which(A1/A2 < 10^-3)} 
-      
-      if (length(idx_prune) >= 1) {
-        W1 = matrix(W1[,-idx_prune], nrow = dim)
-        W2 = matrix(W2[,-idx_prune], nrow = dim)
-        A1 = matrix(A1[,-idx_prune], nrow = 1)
-        A2 = matrix(A2[,-idx_prune], nrow = 1)
-        H1 = matrix(H1[-idx_prune,], ncol = N)
-        H2 = matrix(H2[-idx_prune,], ncol = N)
-      }
-      
-      K = length(A1)
-      
-      score[iter] = if (ncol(A1/A2) > 1) {
-        sum( abs( X - (W1/W2) %*% diag(as.vector(A1/A2)) %*% (H1/H2) ) )
-      } else if (ncol(A1/A2) == 1) {
-        sum( abs( X - (W1/W2) %*% as.vector(A1/A2) %*% (H1/H2) ) )
-      }
-      
-      if (iter %% 100 == 0) {print(paste0("Run Number: ", i, "; Iter Number: ", iter, "; Iter Score: ", round(score[iter], 4)))}
-      if (iter > 1 && abs(score[iter-1] - score[iter]) < 1e-5) {
-        print(paste0('Converged in ', iter,' iterations.'))
-        break} # Convergence criteria!
-    }
-    
-    if( iter == num_iter ) warning('Maximum iterations reached. BNMF did not converge.')
-    end_score[i] = score[tail(which(score != 0),1)]
-    
-    #print(paste0("Run Number: ", i, "; Iter Number: ", iter, "; Final Score: ", round(end_score[i], 4)))
-    
-    # % Among the results, use the fitted variational parameters that achieve the highest ELBO
-    if (i == 1 | (i > 1 && (end_score[i] >= max(end_score)))) {
-      EA = A1/A2
-      EWA = (W1/W2)*diag(A1/A2)
-      EH = H1/H2
-      EW = (W1/W2)
-      varA = A1/(A2^2)
-      varW = W1/(W2^2)
-      alphaH = H1
-      betaH = H2
-    }
-    H_CI_low  <- qgamma(0.025, shape = alphaH, rate = betaH)
-    H_CI_high <- qgamma(0.975, shape = alphaH, rate = betaH)
-  }
-  
-  list(EWA = EWA, EH = EH, H_CI_low = H_CI_low, H_CI_high = H_CI_high)
-}
-
 # Read in Sims
 load("/ifs/scratch/msph/ehs/eag2186/Data/sim_bnmf.RDA")
 
@@ -151,6 +21,10 @@ load("/ifs/scratch/msph/ehs/eag2186/Data/sim_bnmf.RDA")
 ## This only works if run on cluster!!
 job_num = as.integer(Sys.getenv("SGE_TASK_ID"))
 job_num
+
+load("/ifs/scratch/msph/ehs/eag2186/npbnmf/to_do.RDA")
+
+if (!(job_num %in% to_do)) {stop("Job id already ran.")}
 
 # Run everything
 
@@ -178,7 +52,7 @@ get_pca <- function (sim) {
   
   # Cut scores and patterns to rank
   rotations <- as_tibble(rot[, 1:rank])
-  scores <- ex[, 1:rank]
+  scores <- if (rank == 1) {matrix(ex[, 1:rank], nrow = nrow(sim))} else {ex[, 1:rank]}
   
   # Predicted values
   pred <- scores %*% t(rotations) + matrix(rep(apply(sim, 2, mean), each= nrow(scores)), nrow = nrow(scores))
@@ -204,7 +78,7 @@ get_pca_uncenter <- function (sim) {
   
   # Cut scores and patterns to rank
   rotations <- as_tibble(rot[, 1:rank])
-  scores <- ex[, 1:rank]
+  scores <- if (rank == 1) {matrix(ex[, 1:rank], nrow = nrow(sim))} else {ex[, 1:rank]}
   
   # Predicted values
   pred <- scores %*% t(rotations) + matrix(rep(apply(sim, 2, mean), each= nrow(scores)), nrow = nrow(scores))
@@ -250,7 +124,7 @@ match_eigen <- function (truth, loading) {
     corr <- cor(truth, new_loading)
     } else {new_loading <- loading}
   
-  corr <- corr[1:ncol(corr),]
+  corr <- if (ncol(corr) < nrow(corr)) {corr[1:ncol(corr),]} else {corr}
   
   reordered <- matrix(nrow = nrow(new_loading), ncol = ncol(new_loading))
   
@@ -268,7 +142,19 @@ match_eigen <- function (truth, loading) {
     corr[which.max(corr[,2]),] <- NA
     }
   
-  if (ncol(reordered) > 2) {reordered[,which.max(corr[,3])] <- new_loading[,3]}
+  if (ncol(reordered) > 2) {
+    reordered[,which.max(corr[,3])] <- new_loading[,3]
+    corr[which.max(corr[,3]),] <- NA
+  }
+  
+  if (ncol(reordered) > 3) {
+    reordered[,which.max(corr[,4])] <- new_loading[,4]
+    corr[which.max(corr[,4]),] <- NA
+  }
+  
+  if (ncol(reordered) > 4) {
+    reordered[,5:ncol(new_loading)] <- new_loading[,5:ncol(new_loading)]
+  }
   
   reordered
 }
@@ -407,34 +293,6 @@ output_all <- output_all %>%
 
 ## Match Functions
 
-# # If highest correlated eigenvector is negative, flip the whole matrix
-# if (corr[which.max(abs(corr))] < 0) {
-#   new_loading <- -loading
-#   corr <- cor(truth, new_loading)
-# } else {new_loading <- loading}
-# 
-# corr <- corr[1:ncol(corr),]
-# 
-# reordered <- matrix(nrow = nrow(new_loading), ncol = ncol(new_loading))
-# 
-# # Put eigenvector columns in order that best correlates with truth
-# # Put column 1 in column it is most correlated with
-# reordered[,which.max(corr[,1])] <- new_loading[,1]
-# 
-# # Set corresponding ROW to NA so it can't be chosen again
-# corr[which.max(corr[,1]),] <- NA
-# 
-# # Put column 2 in column it is most correlated with
-# if (ncol(reordered) > 1) {
-#   reordered[,which.max(corr[,2])] <- new_loading[,2]
-#   # Set corresponding ROW to NA so it can't be chosen again
-#   corr[which.max(corr[,2]),] <- NA
-# }
-# 
-# if (ncol(reordered) > 2) {reordered[,which.max(corr[,3])] <- new_loading[,3]}
-# 
-# reordered
-
 match_nmf_patterns <- function (truth, loading) {
   truth <- t(truth)
   
@@ -442,34 +300,37 @@ match_nmf_patterns <- function (truth, loading) {
   if (nrow(truth) != nrow(loading)) {loading <- t(as.matrix(loading))} else {loading <- as.matrix(loading)}
   
   corr <- cor(truth, loading)
+  corr <- if (ncol(corr) < nrow(corr)) {corr[1:ncol(corr),]}
   
-  reordered <- matrix(nrow = nrow(loading), ncol = ncol(loading))
+  if (length(corr) != 1) {
+      reordered <- matrix(nrow = nrow(loading), ncol = ncol(loading))
   
-  # Put column 1 in column it is most correlated with
-  reordered[,which.max(corr[,1])] <- loading[,1]
+      # Put column 1 in column it is most correlated with
+      reordered[,which.max(corr[,1])] <- loading[,1]
   
-  # Set corresponding ROW to NA so it can't be chosen again
-  corr[which.max(corr[,1]),] <- NA
+      # Set corresponding ROW to NA so it can't be chosen again
+      corr[which.max(corr[,1]),] <- NA
    
-  # Put column 2 in column it is most correlated with
-  if (ncol(reordered) > 1) {
-     reordered[,which.max(corr[,2])] <- loading[,2]
-     corr[which.max(corr[,2]),] <- NA
-   }
-   
-  if (ncol(reordered) > 2) {
-    reordered[,which.max(corr[,3])] <- loading[,3]
-    corr[which.max(corr[,3]),] <- NA
-    }
-  
-  if (ncol(reordered) > 3) {
-    reordered[,which.max(corr[,4])] <- loading[,4]
-    corr[which.max(corr[,4]),] <- NA
-  }
-  
-  if (ncol(reordered) > 4) {reordered[,which.max(corr[,5])] <- loading[,5]}
-  
-  reordered
+      # Put column 2 in column it is most correlated with
+      if (ncol(reordered) > 1) {
+         reordered[,which.max(corr[,2])] <- loading[,2]
+         corr[which.max(corr[,2]),] <- NA
+       }
+       
+      if (ncol(reordered) > 2) {
+        reordered[,which.max(corr[,3])] <- loading[,3]
+        corr[which.max(corr[,3]),] <- NA
+        }
+      
+      if (ncol(reordered) > 3) {
+        reordered[,which.max(corr[,4])] <- loading[,4]
+        corr[which.max(corr[,4]),] <- NA
+      }
+      
+      if (ncol(reordered) > 4) {reordered[,which.max(corr[,5])] <- loading[,5]}
+  } else {reordered = loading}
+      
+      reordered
 }
 
 match_nmf_scores <- function (rotations_un, rotations, scores_un) {
@@ -550,6 +411,136 @@ output_all <- output_all %>%
 # np BNMF
 #####
 
+##### Read function
+NPBayesNMF <- function(X) {
+  X = as.matrix(X)
+  bnp_switch = 1
+  dim = nrow(X)
+  N = ncol(X)
+  Kinit = ncol(X)
+  
+  nruns = 100
+  end_score = matrix(rep(0, times = nruns))
+  
+  EA = matrix()
+  EWA = matrix()
+  EH = matrix()
+  EW = matrix()
+  
+  for (i in 1:nruns) {
+    
+    K = Kinit
+    
+    w01 = 1
+    w02 = 1
+    
+    W1 = matrix(rgamma(dim*Kinit, shape = dim, scale = 1/dim), nrow = dim, ncol = Kinit)
+    W2 = dim*matrix(1, nrow = dim, ncol = Kinit)
+    
+    a01 = bnp_switch*1/Kinit + (1-bnp_switch)
+    a02 = 1
+    
+    A1 = a01 + bnp_switch*1000*matrix(1, 1, Kinit)/Kinit
+    A2 = a02 + bnp_switch*1000*matrix(1, 1, Kinit)
+    
+    h01 = 1 # Non-sparse prior
+    # h01 = 1/Kinit # Sparse prior
+    h02 = 1
+    
+    H1 = matrix(1, Kinit, N)
+    H2 = matrix(1, Kinit, N)
+    
+    num_iter = 100000
+    
+    score = vector("numeric", length = num_iter)
+    
+    for (iter in 1:num_iter) {
+      
+      EW = W1 / W2
+      
+      # The equivalent of Matlab's repmat(a,2,3) in base R is kronecker(matrix(1,2,3),a)
+      X_reshape = kronecker(array(1, dim = c(K,1,1)), array(t(X), c(1, N, dim)))
+      ElnWA = digamma(W1) - log(W2) + kronecker(array(1, c(dim,1)), digamma(A1)-log(A2))
+      ElnWA_reshape = kronecker(array(1, dim = c(1,N,1)), array(t(ElnWA), c(K, 1, dim)))
+      
+      t1 = array(apply(ElnWA_reshape,c(2,3), max), c(1, Kinit, dim))
+      
+      # % Expected value of log W * expected value of log A
+      ElnWA_reshape = ElnWA_reshape - kronecker(array(1, dim = c(K,1,1)), t1)
+      # % expected value of log H
+      ElnH = digamma(H1) - log(H2)                               
+      
+      P = ElnWA_reshape + kronecker(array(1, dim=c(1,1,dim)), ElnH)
+      P = exp(P)
+      P = P / kronecker(array(1, dim = c(K,1,1)), array(apply(P, 3, colSums), dim = c(1, Kinit, dim)))
+      
+      # % P is a probability to put a lower bound on the ELBO
+      # % expected value of log WAH is concave
+      # % include P to make expectation tractable
+      # % These are update steps from optimizing the ELBO 
+      # % take the gradient with respect to parameter, set to zero, solve
+      
+      H1 = h01 + apply(P * X_reshape, c(1,2), sum)
+      H2 = h02 + t(kronecker(matrix(1, nrow = N, ncol = 1), 
+                             matrix(colSums(EW * kronecker(matrix(1, nrow = dim, ncol = 1), A1/A2)), nrow=1)))
+      
+      W1 = w01 + t(array(apply(X_reshape*P, c(1,3), sum), dim = c(K, dim)))
+      W2 = w02 + kronecker(matrix(1, dim, 1), t(rowSums((H1/H2) * kronecker(matrix(1, 1, N), t(A1/A2)))))
+      
+      A1 = a01 + bnp_switch * t(rowSums(apply(X_reshape*P, c(1,2), sum)))
+      A2 = a02 + bnp_switch * (colSums(W1/W2) * t(rowSums(H1/H2)))
+      
+      # % This is the sparse prior on A, pushing A to zero
+      # If all patterns are zero, don't push anything
+      # prohibits pushing all patterns to zero
+      if ( all(A1/A2 < 10^-3) ) {idx_prune = integer(0)} else {idx_prune = which(A1/A2 < 10^-3)} 
+      
+      if (length(idx_prune) >= 1) {
+        W1 = matrix(W1[,-idx_prune], nrow = dim)
+        W2 = matrix(W2[,-idx_prune], nrow = dim)
+        A1 = matrix(A1[,-idx_prune], nrow = 1)
+        A2 = matrix(A2[,-idx_prune], nrow = 1)
+        H1 = matrix(H1[-idx_prune,], ncol = N)
+        H2 = matrix(H2[-idx_prune,], ncol = N)
+      }
+      
+      K = length(A1)
+      
+      score[iter] = if (ncol(A1/A2) > 1) {
+        sum( abs( X - (W1/W2) %*% diag(as.vector(A1/A2)) %*% (H1/H2) ) )
+      } else if (ncol(A1/A2) == 1) {
+        sum( abs( X - (W1/W2) %*% as.vector(A1/A2) %*% (H1/H2) ) )
+      }
+      
+      if (iter %% 100 == 0) {print(paste0("Run Number: ", i, "; Iter Number: ", iter, "; Iter Score: ", round(score[iter], 4)))}
+      if (iter > 1 && abs(score[iter-1] - score[iter]) < 1e-5) {
+        print(paste0('Converged in ', iter,' iterations.'))
+        break} # Convergence criteria!
+    }
+    
+    if( iter == num_iter ) warning('Maximum iterations reached. BNMF did not converge.')
+    end_score[i] = score[tail(which(score != 0),1)]
+    
+    #print(paste0("Run Number: ", i, "; Iter Number: ", iter, "; Final Score: ", round(end_score[i], 4)))
+    
+    # % Among the results, use the fitted variational parameters that achieve the highest ELBO
+    if (i == 1 | (i > 1 && (end_score[i] >= max(end_score)))) {
+      EA = A1/A2
+      EWA = (W1/W2)*diag(A1/A2)
+      EH = H1/H2
+      EW = (W1/W2)
+      varA = A1/(A2^2)
+      varW = W1/(W2^2)
+      alphaH = H1
+      betaH = H2
+    }
+    H_CI_low  <- qgamma(0.025, shape = alphaH, rate = betaH)
+    H_CI_high <- qgamma(0.975, shape = alphaH, rate = betaH)
+  }
+  
+  list(EWA = EWA, EH = EH, H_CI_low = H_CI_low, H_CI_high = H_CI_high)
+}
+
 get_bnmf <- function (sim) {
   set.seed(1988)
   bnmf_out <- NPBayesNMF(sim)
@@ -573,6 +564,7 @@ output_all <- output_all %>%
   mutate(bnmf_loadings = map2(true_patterns, bnmf_loadings_un, match_nmf_patterns),
          bnmf_scores = pmap(list(bnmf_loadings_un, bnmf_loadings, bnmf_scores_un), 
                              match_nmf_scores))
+
 
 ###############################
 # Symmetric Subspace Distance #
