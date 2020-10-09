ae24abd1a0d15cdf01cda3c6164ff06477cfadf4
lizzyagibson <eag2186@cumc.columbia.edu>
2020-10-09 19:34:41 -0400

    Double check symmetric subspace distance.

--- R/symm_sub_dist.R
+++ R/symm_sub_dist.R
@@ -6,24 +6,6 @@
 #V <- matrix(rnorm(1:60), ncol = 6)
 #S <- U + 0.1
 
-## base
-qr(U) # upper triangle is R, lower triangle is info on Q...
-## qy.qr(): return the results of the matrix multiplications: Q %*% y, 
-## where Q is the order-nrow(x) orthogonal (or unitary) transformation represented by qr.
-qr.Q(qr(U))
-## Normal!!
-apply(qr.Q(qr(U)), 2, function(x) sqrt(sum(x^2)))
-
-svd(U)$u
-apply(svd(U)$u, 2, function(x) sqrt(sum(x^2)))
-
-## Orthogonal!! + Normal = ORTHONORMAL
-t(qr.Q(qr(U))) %*% (qr.Q(qr(U)))
-
-## Orthogonal!! + Normal = ORTHONORMAL
-t(svd(U)$u) %*% (svd(U)$u)
-
-
 ### Symmetric Subspace Distance
 symm_subspace_dist <- function(U, V) {
   
@@ -37,11 +19,8 @@ symm_subspace_dist <- function(U, V) {
   n <- ncol(V)
   
   dUV <- sqrt( max(m,n) - sum((t(qrU) %*% qrV)^2) )
-  
   ratio <- dUV/sqrt( max(m,n))
-  
   ratio
-  
 }
 
 symm_subspace_dist(U,V)
