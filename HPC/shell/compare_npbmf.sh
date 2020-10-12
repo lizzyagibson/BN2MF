3fa866ca2ab9e0e8463062cc438325d40d5c6869
lizzyagibson <eag2186@cumc.columbia.edu>
2020-10-12 14:21:13 -0400

    Edit shell script.

--- HPC/shell/compare_npbmf.sh
+++ HPC/shell/compare_npbmf.sh
@@ -2,7 +2,7 @@
 #$ -cwd -S /bin/bash
 #$ -o /dev/hull
 #$ -e /dev/null
-#$ -l mem=20G
+#$ -l mem=12G
 #$ -l time=:10000:
 #$ -M eag2186@cumc.columbia.edu
 
@@ -11,6 +11,6 @@ module load R/3.6.0
 
 clear
 
-R CMD BATCH --no-save everything_on_cluster.R
+R CMD BATCH --no-save everything_on_cluster_unstand.R
 
 
