4b6c39f2ed5acf51e687f0421157ec80e6ae86fc
lizzyagibson <eag2186@cumc.columbia.edu>
2020-10-25 09:22:20 -0400

    Add figures to dgp sim results.

--- HPC/Code/dgp_on_loca.R
+++ HPC/Code/dgp_on_loca.R
@@ -91,7 +91,12 @@ get_fa <- function (sim) {
 
 ## Run
 
-output_all <- output_all %>% 
+output_all[136,] %>% 
+  mutate(fa_out       = map(sim, get_fa))
+
+# 114, 136, 172, 196, 197: x system is computationally singular: reciprocal condition number
+
+output_fa <- output_all[c(1:113, 115:135, 137:171, 173:195, 198:200),] %>% 
   mutate(fa_out       = map(sim, get_fa),
          fa_rotations = map(fa_out, function(x) x[[1]]),
          fa_scores    = map(fa_out, function(x) x[[2]]),
@@ -234,3 +239,122 @@ output_all <- output_all %>%
 dgp_all <- output_all %>% dplyr::select(-grep("_out", colnames(.)))
 
 # save(output_all, file = paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/dgp_out/dgp_sims", job_num, ".RDA"))
+
+#####
+# Viz
+
+# Relative Error
+dgp_e <- dgp_all %>% 
+  dplyr::select(seed, data, sim, chem, grep("pred", colnames(.))) %>% 
+  pivot_longer(c(pca_pred:nmf_p_pred, fa_pred)) %>% 
+  mutate(l2_true = map2(chem, value, function (x,y) norm(x-y, "F")/norm(x, "F")),
+         l2_sim = map2(sim, value, function (x,y) norm(x-y, "F")/norm(x, "F")),
+         name = str_remove(name, "_pred")) %>% 
+  dplyr::select(seed, data, name, l2_true, l2_sim) %>% 
+  unnest(c(l2_sim, l2_true))
+
+dgp_e %>%
+  ggplot(aes(x = name, y = l2_sim, color = name, fill = name)) +
+  geom_boxplot(alpha = 0.5) +
+  facet_grid(. ~ data, scales = "free") + 
+  geom_vline(xintercept = 0, color = "pink", linetype = "dashed", size = 0.5) +
+  scale_y_log10() +
+  theme(legend.position = "none") + 
+  labs(y = "Relative Predictive Error",
+       title = "vs SIMS")
+
+dgp_e %>%
+  ggplot(aes(x = name, y = l2_true, color = name, fill = name)) +
+  geom_boxplot(alpha = 0.5) +
+  facet_grid(. ~ data, scales = "free") + 
+  geom_vline(xintercept = 0, color = "pink", linetype = "dashed", size = 0.5) +
+  scale_y_log10() +
+  theme(legend.position = "none") + 
+  labs(y = "Relative Predictive Error",
+       title = "vs PRE NOISE TRUTH")
+
+dgp_s <- dgp_all %>% dplyr::select(seed, data, grep("_ssdist", colnames(.))) %>% 
+  unnest(cols = grep("_ssdist", colnames(.))) %>% 
+  pivot_longer(grep("_ssdist", colnames(.)),
+               names_to = "type",
+               values_to = "value") %>%
+  mutate(model = str_sub(type, 1, 6),
+         model = ifelse(str_detect(model, 'pca'), 'PCA', model),
+         model = ifelse(str_detect(model, 'l2'), 'L2 NMF', model),
+         model = ifelse(str_detect(model, 'fa'), 'FA', model),
+         model = ifelse(str_detect(model, '_p_'), 'Poisson NMF', model),
+         model = ifelse(str_detect(model, 'bnmf'), 'BNMF', model),
+         type = ifelse(str_detect(type, 'scores'), 'Scores', "Loadings"))
+
+dgp_s %>% 
+  ggplot(aes(x = model, y = value, color = model)) +
+  geom_boxplot() +
+  facet_grid(data ~ type) + 
+  geom_hline(yintercept = 0.5, color = "pink", linetype = "dashed", size = 0.5) +
+  theme(legend.position = "none") + 
+  # scale_y_log10() +
+  labs(y = "Symmetric Subspace Distance")
+
+#####
+# Rank
+#####
+
+dgp_data %>%
+  dplyr::select(seed, data, fa_rank, pca_rank, nmf_l2_rank, nmf_p_rank, bnmf_rank) %>%
+  unnest(c(fa_rank, pca_rank, nmf_l2_rank, nmf_p_rank, bnmf_rank)) %>%
+  pivot_longer(cols = fa_rank:bnmf_rank) %>%
+  mutate(value = ifelse(value > 5, "> 5", value)) %>% 
+  group_by(name, value, data) %>%
+  summarise(n = n()) %>% 
+  ungroup() %>% 
+  filter(data == "Distinct") %>% 
+  dplyr::select(-data) %>% 
+  pivot_wider(names_from = value,
+              values_from = n) %>% 
+  mutate_if(is.integer, replace_na, 0)
+
+dgp_data %>%
+  dplyr::select(seed, data, fa_rank, pca_rank, nmf_l2_rank, nmf_p_rank, bnmf_rank) %>%
+  unnest(c(fa_rank, pca_rank, nmf_l2_rank, nmf_p_rank, bnmf_rank)) %>%
+  pivot_longer(cols = fa_rank:bnmf_rank) %>%
+  mutate(value = ifelse(value > 5, "> 5", value)) %>% 
+  group_by(name, value, data) %>%
+  summarise(n = n()) %>% 
+  ungroup() %>% 
+  filter(data == "Overlapping") %>% 
+  dplyr::select(-data) %>% 
+  pivot_wider(names_from = value,
+              values_from = n) %>% 
+  mutate_if(is.integer, replace_na, 0)
+
+dgp_all %>%
+  dplyr::select(seed, data, fa_rank, pca_rank, nmf_l2_rank, nmf_p_rank) %>%
+  unnest(c(fa_rank, pca_rank, nmf_l2_rank, nmf_p_rank)) %>%
+  pivot_longer(cols = fa_rank:nmf_p_rank) %>%
+  mutate(value = ifelse(value > 5, "> 5", value)) %>% 
+  group_by(name, value, data) %>%
+  summarise(n = n()) %>% 
+  ungroup() %>% 
+  filter(data == "Distinct") %>% 
+  dplyr::select(-data) %>% 
+  pivot_wider(names_from = value,
+              values_from = n) %>% 
+  mutate_if(is.integer, replace_na, 0) %>% 
+  dplyr::select(name, `1`,`2`,`3`,`4`, `5`) %>% 
+  knitr::kable(caption = "Distinct Simulations: Patterns Identified")
+
+dgp_all %>%
+  dplyr::select(seed, data, fa_rank, pca_rank, nmf_l2_rank, nmf_p_rank) %>%
+  unnest(c(fa_rank, pca_rank, nmf_l2_rank, nmf_p_rank)) %>%
+  pivot_longer(cols = fa_rank:nmf_p_rank) %>%
+  mutate(value = ifelse(value > 5, "> 5", value)) %>% 
+  group_by(name, value, data) %>%
+  summarise(n = n()) %>% 
+  ungroup() %>% 
+  filter(data == "Overlapping") %>% 
+  dplyr::select(-data) %>% 
+  pivot_wider(names_from = value,
+              values_from = n) %>% 
+  mutate_if(is.integer, replace_na, 0) %>% 
+  dplyr::select(name, `1`, `2`, `3`, `4`, `5`) %>% 
+  knitr::kable(caption = "Overlapping Simulations: Patterns Identified")
