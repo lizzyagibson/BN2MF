7d75c5349f1b455cf20cde0d8dc357955a14a9a5
lizzyagibson <eag2186@cumc.columbia.edu>
2020-11-06 17:35:44 -0500

    Comment out completed steps.

--- HPC/Code/dgp_on_loca.R
+++ HPC/Code/dgp_on_loca.R
@@ -33,12 +33,12 @@ summary(apply(dist, 2, sd))
 #####
 # PCA
 #####
-dgp_local <- sim_dgp_local %>% 
-  mutate(pca_out       = map(sim, get_pca),
-         pca_rotations = map(pca_out, function(x) x[[1]]),
-         pca_scores    = map(pca_out, function(x) x[[2]]),
-         pca_pred      = map(pca_out, function(x) x[[3]]),
-         pca_rank      = map(pca_out, function(x) x[[4]]))
+# dgp_local <- sim_dgp_local %>% 
+#   mutate(pca_out       = map(sim, get_pca),
+#          pca_rotations = map(pca_out, function(x) x[[1]]),
+#          pca_scores    = map(pca_out, function(x) x[[2]]),
+#          pca_pred      = map(pca_out, function(x) x[[3]]),
+#          pca_rank      = map(pca_out, function(x) x[[4]]))
 
 #####
 # Factor Analysis
@@ -46,91 +46,99 @@ dgp_local <- sim_dgp_local %>%
 
 # Some sims computationally singular
 # Rows: 114, 136, 172, 196, 197
-fa_local <- dgp_local[c(1:113, 115:135, 137:171, 173:195, 198:200),] %>% 
-  mutate(fa_out       = map(sim, get_fa),
-         fa_rotations = map(fa_out, function(x) x[[1]]),
-         fa_scores    = map(fa_out, function(x) x[[2]]),
-         fa_pred      = map(fa_out, function(x) x[[3]]),
-         fa_rank      = map(fa_out, function(x) x[[4]]))
-
-dgp_local <- left_join(dgp_local, fa_local)
+# fa_local <- dgp_local[c(1:113, 115:135, 137:171, 173:195, 198:200),] %>% 
+#   mutate(fa_out       = map(sim, get_fa),
+#          fa_rotations = map(fa_out, function(x) x[[1]]),
+#          fa_scores    = map(fa_out, function(x) x[[2]]),
+#          fa_pred      = map(fa_out, function(x) x[[3]]),
+#          fa_rank      = map(fa_out, function(x) x[[4]]))
+# 
+# dgp_local <- left_join(dgp_local, fa_local)
 
 #####
 # L2 NMF
 #####
-dgp_local <- dgp_local %>%
-  mutate(nmf_l2_out      = map(sim, get_nmf_l2),
-         nmf_l2_loadings = map(nmf_l2_out, function(x) x[[1]]),
-         nmf_l2_scores   = map(nmf_l2_out, function(x) x[[2]]),
-         nmf_l2_pred     = map(nmf_l2_out, function(x) x[[3]]),
-         nmf_l2_rank     = map(nmf_l2_out, function(x) x[[4]]))
+# dgp_local <- dgp_local %>%
+#   mutate(nmf_l2_out      = map(sim, get_nmf_l2),
+#          nmf_l2_loadings = map(nmf_l2_out, function(x) x[[1]]),
+#          nmf_l2_scores   = map(nmf_l2_out, function(x) x[[2]]),
+#          nmf_l2_pred     = map(nmf_l2_out, function(x) x[[3]]),
+#          nmf_l2_rank     = map(nmf_l2_out, function(x) x[[4]]))
 
 #####
 # Poisson NMF
 #####
-dgp_local <- dgp_local %>% 
-  mutate(nmf_p_out      = map(sim, get_nmf_p),
-         nmf_p_loadings = map(nmf_p_out, function(x) x[[1]]),
-         nmf_p_scores   = map(nmf_p_out, function(x) x[[2]]),
-         nmf_p_pred     = map(nmf_p_out, function(x) x[[3]]),
-         nmf_p_rank     = map(nmf_p_out, function(x) x[[4]]))
+# dgp_local <- dgp_local %>% 
+#   mutate(nmf_p_out      = map(sim, get_nmf_p),
+#          nmf_p_loadings = map(nmf_p_out, function(x) x[[1]]),
+#          nmf_p_scores   = map(nmf_p_out, function(x) x[[2]]),
+#          nmf_p_pred     = map(nmf_p_out, function(x) x[[3]]),
+#          nmf_p_rank     = map(nmf_p_out, function(x) x[[4]]))
 
 #####
 # Symmetric Subspace Distance
 #####
-dgp_local <- dgp_local %>% 
-  mutate(pca_norm              = map2(sim, pca_pred,    function(x,y) norm(x-y, "F")/norm(x, "F")),
-         nmf_l2_norm           = map2(sim, nmf_l2_pred, function(x,y) norm(x-y, "F")/norm(x, "F")),
-         nmf_p_norm            = map2(sim, nmf_p_pred,  function(x,y) norm(x-y, "F")/norm(x, "F")),
-         pca_rotation_ssdist   = map2(true_patterns, pca_rotations,   symm_subspace_dist),
-         pca_scores_ssdist     = map2(true_scores,   pca_scores,      symm_subspace_dist),
-         nmf_l2_loading_ssdist = map2(true_patterns, nmf_l2_loadings, symm_subspace_dist),
-         nmf_l2_scores_ssdist  = map2(true_scores,   nmf_l2_scores,   symm_subspace_dist),
-         nmf_p_loading_ssdist  = map2(true_patterns, nmf_p_loadings,  symm_subspace_dist),
-         nmf_p_scores_ssdist   = map2(true_scores,   nmf_p_scores,    symm_subspace_dist))
-
-fa_metrics <- dgp_local[c(1:113, 115:135, 137:171, 173:195, 198:200),] %>% 
-  mutate(fa_norm               = map2(sim, fa_pred,     function(x,y) norm(x-y, "F")/norm(x, "F")),
-         fa_rotations_ssdist   = map2(true_patterns, fa_rotations,    symm_subspace_dist),
-         fa_scores_ssdist      = map2(true_scores,   fa_scores,       symm_subspace_dist))
-
-dgp_local <- left_join(dgp_local, fa_metrics)
+# dgp_local <- dgp_local %>% 
+#   mutate(pca_norm              = map2(sim, pca_pred,    function(x,y) norm(x-y, "F")/norm(x, "F")),
+#          nmf_l2_norm           = map2(sim, nmf_l2_pred, function(x,y) norm(x-y, "F")/norm(x, "F")),
+#          nmf_p_norm            = map2(sim, nmf_p_pred,  function(x,y) norm(x-y, "F")/norm(x, "F")),
+#          pca_rotation_ssdist   = map2(true_patterns, pca_rotations,   symm_subspace_dist),
+#          pca_scores_ssdist     = map2(true_scores,   pca_scores,      symm_subspace_dist),
+#          nmf_l2_loading_ssdist = map2(true_patterns, nmf_l2_loadings, symm_subspace_dist),
+#          nmf_l2_scores_ssdist  = map2(true_scores,   nmf_l2_scores,   symm_subspace_dist),
+#          nmf_p_loading_ssdist  = map2(true_patterns, nmf_p_loadings,  symm_subspace_dist),
+#          nmf_p_scores_ssdist   = map2(true_scores,   nmf_p_scores,    symm_subspace_dist))
+# 
+# fa_metrics <- dgp_local[c(1:113, 115:135, 137:171, 173:195, 198:200),] %>% 
+#   mutate(fa_norm               = map2(sim, fa_pred,     function(x,y) norm(x-y, "F")/norm(x, "F")),
+#          fa_rotations_ssdist   = map2(true_patterns, fa_rotations,    symm_subspace_dist),
+#          fa_scores_ssdist      = map2(true_scores,   fa_scores,       symm_subspace_dist))
+# 
+# dgp_local <- left_join(dgp_local, fa_metrics)
 
 #####
 # Save
 #####
-dgp_local <- dgp_local %>% dplyr::select(-grep("_out", colnames(.)))
+# dgp_local <- dgp_local %>% dplyr::select(-grep("_out", colnames(.)))
 # save(dgp_local, file = "./HPC/Rout/dgp_local.RDA")
 load( "./HPC/Rout/dgp_local.RDA")
 dgp_local
 
+all_equal(dgp_local$sim[[1]], sim_dgp_local$sim[[1]])
+
 #####
 # Add MATLAB
-# dgp_bnmf <- tibble()
-# 
-# for (i in 1:200) {
-#     eh <- readMat(here::here(paste0("/MATLAB/dgp_local/eh_dist_", i, ".mat")))[[1]]
-#     eh <- as_tibble(eh) %>% nest(eh = everything()) %>% mutate(seed = i)
-#     
-#     ewa <- readMat(here::here(paste0("/MATLAB/dgp_local/ewa_dist_", i, ".mat")))[[1]]
-#     ewa <- as_tibble(ewa) %>% nest(ewa = everything()) %>% mutate(seed = i)
-#     both <- full_join(eh, ewa, by = "seed")
-#     dgp_bnmf <- rbind(dgp_bnmf, both) %>% drop_na(seed)
-#   }
+dgp_bnmf <- tibble()
 
-# dgp_local <- left_join(dgp_local, dgp_bnmf, by = "seed") %>% 
-#   mutate(bnmf_pred            = map2(ewa, eh, function(x,y) as.matrix(x) %*% as.matrix(y)),
-#          bnmf_norm            = map2(sim, bnmf_pred,    function(x,y) norm(x-y, "F")/norm(x, "F")),
-#          bnmf_loading_ssdist  = map2(true_patterns, eh,  symm_subspace_dist),
-#          bnmf_scores_ssdist   = map2(true_scores,   ewa,    symm_subspace_dist),
-#          bnmf_rank = map(eh, nrow))
+for (i in 1:200) {
+    eh <- readMat(here::here(paste0("MATLAB/dgp_local/eh_dist_", i, ".mat")))[[1]]
+    eh <- as_tibble(eh) %>% nest(eh = everything()) %>% mutate(row = i)
+
+    ewa <- readMat(here::here(paste0("./MATLAB/dgp_local/ewa_dist_", i, ".mat")))[[1]]
+    ewa <- as_tibble(ewa) %>% nest(ewa = everything()) %>% mutate(row = i)
+    both <- full_join(eh, ewa, by = "row")
+    dgp_bnmf <- rbind(dgp_bnmf, both)
+  }
+
+dgp_bnmf <- dgp_bnmf %>% 
+  mutate(seed = rep(1:100, 2),
+         data = c(rep("Distinct", 100), rep("Overlapping", 100)))
+
+dgp <- 
+  dgp_local %>% 
+  dplyr::select(-eh, -ewa, -grep("bnmf", colnames(.))) %>% 
+  left_join(., dgp_bnmf, by = c("seed", "data")) %>%
+  mutate(bnmf_pred            = map2(ewa, eh, function(x,y) as.matrix(x) %*% as.matrix(y)),
+         bnmf_loading_ssdist  = map2(true_patterns, eh,  symm_subspace_dist),
+         bnmf_scores_ssdist   = map2(true_scores,   ewa,    symm_subspace_dist),
+         bnmf_rank = map(eh, nrow))
 
 #####
 # Viz
 #####
 
 # Relative Error
-dgp_e <- dgp_local %>% 
+dgp_e <- dgp %>% 
   dplyr::select(seed, data, sim, chem, grep("pred", colnames(.))) %>% 
   pivot_longer(c(pca_pred:bnmf_pred)) %>% 
   drop_na(value) %>% 
@@ -141,7 +149,8 @@ dgp_e <- dgp_local %>%
   unnest(c(l2_sim, l2_true))
 
 # Subspace Distance
-dgp_s <- dgp_local %>% dplyr::select(seed, data, grep("_ssdist", colnames(.))) %>% 
+dgp_s <- dgp %>% 
+  dplyr::select(seed, data, grep("_ssdist", colnames(.))) %>% 
   unnest(cols = grep("_ssdist", colnames(.))) %>% 
   pivot_longer(grep("_ssdist", colnames(.)),
                names_to = "type",
@@ -156,7 +165,7 @@ dgp_s <- dgp_local %>% dplyr::select(seed, data, grep("_ssdist", colnames(.))) %
          type = ifelse(str_detect(type, 'scores'), 'Scores', "Loadings"))
 
 # Rank
-rank_dist <- dgp_local %>%
+rank_dist <- dgp %>%
   dplyr::select(seed, data, grep("rank", colnames(.))) %>%
   unnest(c(pca_rank:bnmf_rank)) %>%
   pivot_longer(cols = pca_rank:bnmf_rank) %>%
@@ -172,7 +181,7 @@ rank_dist <- dgp_local %>%
   # dplyr::select(name, `1`,`2`,`3`,`4`, `5`) %>% 
   knitr::kable(caption = "Distinct Simulations: Patterns Identified")
 
-rank_over <- dgp_local %>%
+rank_over <- dgp %>%
   dplyr::select(seed, data, grep("rank", colnames(.))) %>%
   unnest(c(pca_rank:bnmf_rank)) %>%
   pivot_longer(cols = pca_rank:bnmf_rank) %>%
@@ -219,10 +228,56 @@ dgp_matrix_norms <-
                          grepl("ewa|eh", results) ~ "BN2MF",
                          grepl("pca", results) ~ "PCA"))
 
+pca_4 <- dgp_local %>% 
+  dplyr::select(seed, data, true_patterns, true_scores, 
+                pca_rotations, pca_scores, pca_rank) %>% 
+  mutate(true_patterns = map(true_patterns, t),
+         model = "PCA") %>% 
+  rename(pred_load = 5, pred_score = 6, rank = 7)
+
+fa_4 <- dgp_local %>% 
+  dplyr::select(seed, data, true_patterns, true_scores, 
+                fa_rotations, fa_scores, fa_rank) %>% 
+  mutate(true_patterns = map(true_patterns, t),
+         model = "FA") %>% 
+  rename(pred_load = 5, pred_score = 6, rank = 7)
+  
+nmf_l2_4 <- dgp_local %>% 
+  dplyr::select(seed, data, true_patterns, true_scores,
+                nmf_l2_loadings, nmf_l2_scores, nmf_l2_rank) %>% 
+  mutate(true_patterns = map(true_patterns, t),
+         model = "NMF L2") %>% 
+  rename(pred_load = 5, pred_score = 6, rank = 7)
+
+nmf_p_4 <- dgp_local %>% 
+  dplyr::select(seed, data, true_patterns, true_scores, 
+                nmf_p_loadings, nmf_p_scores, nmf_p_rank) %>% 
+  mutate(true_patterns = map(true_patterns, t),
+         model = "NMF P") %>% 
+  rename(pred_load = 5, pred_score = 6, rank = 7)
+
+bnmf_4 <- dgp_local %>% 
+  dplyr::select(seed, data, true_patterns, true_scores, 
+                eh, ewa, bnmf_rank) %>% 
+  mutate(true_patterns = map(true_patterns, t),
+         model = "BN2MF") %>% 
+  rename(pred_load = 5, pred_score = 6, rank = 7)
+
+norm_plot <- rbind(pca_4, fa_4, nmf_l2_4, nmf_p_4, bnmf_4) %>% 
+  unnest(rank) %>% 
+  filter(rank == 4) %>%
+  mutate(pred_load = map(pred_load, function(x) if (nrow(x) != 50) {t(x)} else{x}),
+         norm_load = map2(true_patterns, pred_load, 
+                           function(x,y) norm(as.matrix(x)-as.matrix(y), "F")/norm(as.matrix(x))),
+         pred_score = map(pred_score, function(x) if (nrow(x) != 1000) {t(x)} else{x}),
+         norm_score = map2(true_scores, pred_score, 
+                           function(x,y) norm(as.matrix(x)-as.matrix(y), "F")/norm(as.matrix(x)))) %>% 
+  unnest(c(norm_load, norm_score)) %>% 
+  dplyr::select(seed, data, model, norm_load, norm_score)
+
 #####
 # Look
 #####
-source("./R/fig_set.R")
 
 # Relative Error
 dgp_e %>%
@@ -236,7 +291,7 @@ dgp_e %>%
 
 # Subspace Distance
 dgp_s %>% 
-  ggplot(aes(x = model, y = value, fill = model)) +
+  ggplot(aes(x = model, y = value, color = model, fill = model)) +
   geom_boxplot(alpha = 0.5) +
   facet_grid(data ~ type) + 
   geom_hline(yintercept = 0.5, color = "pink", linetype = "dashed", size = 0.5) +
@@ -254,6 +309,14 @@ dgp_matrix_norms %>%
   scale_y_log10() +
   labs(y = "Relative Predictive Error",
        title = "")
+
+norm_plot %>% 
+  filter(model != "NMF L2") %>%
+  # filter(model != "NMF P") %>% 
+  ggplot(aes(x = model, y = value, color = model, fill = model)) +
+  geom_jitter(alpha = 0.5, height = 0, width = .3) +
+  geom_boxplot(alpha = 0.75, notch = TRUE, outlier.size = 0, varwidth = TRUE) +
+  facet_grid(name ~ data)
 
 rank_dist
 rank_over
