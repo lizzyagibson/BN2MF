a72e90044808025f09be2edd30fc80019e3c26b4
lizzyagibson <eag2186@cumc.columbia.edu>
2020-12-08 16:17:10 -0500

    Remove unnesting to run on hpc.

--- HPC/Code/combine_dim.R
+++ HPC/Code/combine_dim.R
@@ -9,23 +9,38 @@ job_num
 
 ## Get functions
 source("/ifs/scratch/msph/ehs/eag2186/npbnmf/compare_functions.R")
+# Packages
+library(tidyverse)
+library(CVXR, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+
+## read job number from system environment
+## This only works if run on cluster!!
+job_num = as.integer(Sys.getenv("SGE_TASK_ID"))
+job_num
+
+# Files to do
+#load("/ifs/scratch/msph/ehs/eag2186/npbnmf/to_do_dim.RDA")
+#if (!(job_num %in% to_do)) {quit("no")}
+
+## Get functions
+source("/ifs/scratch/msph/ehs/eag2186/npbnmf/compare_functions.R")
 source("/ifs/scratch/msph/ehs/eag2186/npbnmf/factor_correspondence.R")
 
-load(paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/dim_out/dim_out", job_num, ".RDA")) 
-#load("/Users/lizzy/BN2MF/HPC/Rout/dim_out/dim_out_1_ex.RDA") 
+load(paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/dim_out/dim_out_", job_num, ".RDA"))
+#load("/Users/lizzy/BN2MF/HPC/Rout/dim_out/dim_out_1_ex.RDA")
 #output_all
 
 # Calculate all error metrics
 
-output_all <- output_all %>% 
-  mutate(pca_l2er  = map2(chem,   pca_pred,   
-                        function(x,y) norm(x-y, "F")/norm(x, "F") ),
+output_all <- output_all %>%
+  mutate(pca_l2er  = map2(chem,   pca_pred,
+                          function(x,y) norm(x-y, "F")/norm(x, "F") ),
          fa_l2er   = map2(chem,   fa_pred,
-                        function(x,y) norm(x-y, "F")/norm(x, "F") ),
-         nmfl2_l2er = map2(chem,  nmfl2_pred, 
-                        function(x,y) norm(x-y, "F")/norm(x, "F") ),
+                          function(x,y) norm(x-y, "F")/norm(x, "F") ),
+         nmfl2_l2er = map2(chem,  nmfl2_pred,
+                           function(x,y) norm(x-y, "F")/norm(x, "F") ),
          nmfp_l2er   = map2(chem, nmfp_pred,
-                        function(x,y) norm(x-y, "F")/norm(x, "F") ))
+                            function(x,y) norm(x-y, "F")/norm(x, "F") ))
 
 output_all <- output_all %>%
   mutate(true_patterns   = map(true_patterns, t),
@@ -33,76 +48,73 @@ output_all <- output_all %>%
          nmfp_loadings   = map(nmfp_loadings, t),
          true_patterns = map(true_patterns, as.matrix),
          pca_loadings = map(pca_loadings, as.matrix)) %>%
-  mutate(pca_perm = map2(true_patterns, pca_loadings, 
-                         function(x,y) if(ncol(y) == 4)
+  mutate(pca_perm = map2(true_patterns, pca_loadings,
+                         function(x,y) if(ncol(y) == ncol(x))
                          {factor_correspondence(as.matrix(x),
                                                 as.matrix(y), nn = FALSE)$permutation_matrix} else{NA}),
-         pca_loadings_re = map2(pca_loadings, pca_perm, 
-                                function(x,y) if(ncol(x) == 4) 
+         pca_loadings_re = map2(pca_loadings, pca_perm,
+                                function(x,y) if(!any(is.na(y)))
                                 {x %*% y} else{NA}),
-         pca_scores_re = map2(pca_scores, pca_perm, 
-                              function(x,y) if(ncol(x) == 4) 
+         pca_scores_re = map2(pca_scores, pca_perm,
+                              function(x,y) if(!any(is.na(y)))
                               {x %*% y} else{NA}),
-         fa_perm = map2(true_patterns, fa_loadings, 
-                         function(x,y) if(ncol(y) == 4)
-                         {factor_correspondence(as.matrix(x),
-                                                as.matrix(y), nn = FALSE)$permutation_matrix} else{NA}),
-         fa_loadings_re = map2(fa_loadings, fa_perm, 
-                                function(x,y) if(ncol(x) == 4) 
-                                {x %*% y} else{NA}),
-         fa_scores_re = map2(fa_scores, fa_perm, 
-                              function(x,y) if(ncol(x) == 4) 
-                              {x %*% y} else{NA}),
+         fa_perm = map2(true_patterns, fa_loadings,
+                        function(x,y) if(ncol(y) == ncol(x))
+                        {factor_correspondence(as.matrix(x),
+                                               as.matrix(y), nn = FALSE)$permutation_matrix} else{NA}),
+         fa_loadings_re = map2(fa_loadings, fa_perm,
+                               function(x,y) if(!any(is.na(y)))
+                               {x %*% y} else{NA}),
+         fa_scores_re = map2(fa_scores, fa_perm,
+                             function(x,y) if(!any(is.na(y)))
+                             {x %*% y} else{NA}),
          nmfl2_perm = map2(true_patterns, nmfl2_loadings,
-                           function(x,y) if(ncol(y) == 4)
+                           function(x,y) if(ncol(y) == ncol(x))
                            {factor_correspondence(as.matrix(x),
                                                   as.matrix(y))$permutation_matrix} else{NA}),
-         nmfl2_loadings_re = map2(nmfl2_loadings, nmfl2_perm, 
-                                  function(x,y) if(ncol(x) == 4) 
+         nmfl2_loadings_re = map2(nmfl2_loadings, nmfl2_perm,
+                                  function(x,y) if(!any(is.na(y)))
                                   {x %*% y} else{NA}),
          nmfl2_scores_re = map2(nmfl2_scores, nmfl2_perm,
-                                function(x,y) if(ncol(x) == 4) 
+                                function(x,y) if(!any(is.na(y)))
                                 {x %*% y} else{NA}),
          nmfp_perm = map2(true_patterns, nmfp_loadings,
-                          function(x,y) if(ncol(y) == 4)
+                          function(x,y) if(ncol(y) == ncol(x))
                           {factor_correspondence(as.matrix(x),
                                                  as.matrix(y))$permutation_matrix} else{NA}),
-         nmfp_loadings_re = map2(nmfp_loadings, nmfp_perm, 
-                                 function(x,y) if(ncol(x) == 4) 
+         nmfp_loadings_re = map2(nmfp_loadings, nmfp_perm,
+                                 function(x,y) if(!any(is.na(y)))
                                  {x %*% y} else{NA}),
-         nmfp_scores_re = map2(nmfp_scores, nmfp_perm, 
-                               function(x,y) if(ncol(x) == 4) 
+         nmfp_scores_re = map2(nmfp_scores, nmfp_perm,
+                               function(x,y) if(!any(is.na(y)))
                                {x %*% y} else{NA}))
-         
-output_all <- 
-  output_all %>% 
-  mutate(pca_load_l2 = map2(true_patterns, pca_loadings_re, 
-                            function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA}),
-         pca_score_l2 = map2(true_scores, pca_scores_re, 
-                            function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA}),
-        fa_load_l2 = map2(true_patterns, fa_loadings_re,
-                           function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA}),
-        fa_score_l2 = map2(true_scores, fa_scores_re,
-                           function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA}),
-        nmfl2_load_l2 = map2(true_patterns, nmfl2_loadings_re,
-                           function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA}),
-         nmfl2_score_l2 = map2(true_scores, nmfl2_scores_re,
-                             function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA}),
-         nmfp_load_l2 = map2(true_patterns, nmfp_loadings_re,
-                            function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA}),
-         nmfp_score_l2 = map2(true_scores, nmfp_scores_re,
-                             function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA})
-        )
 
-output_all <- output_all %>% 
+output_all <-
+  output_all %>%
+  mutate(pca_load_l2 = map2(true_patterns, pca_loadings_re,
+                            function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA}),
+         pca_score_l2 = map2(true_scores, pca_scores_re,
+                             function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA}),
+         fa_load_l2 = map2(true_patterns, fa_loadings_re,
+                           function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA}),
+         fa_score_l2 = map2(true_scores, fa_scores_re,
+                            function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA}),
+         nmfl2_load_l2 = map2(true_patterns, nmfl2_loadings_re,
+                              function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA}),
+         nmfl2_score_l2 = map2(true_scores, nmfl2_scores_re,
+                               function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA}),
+         nmfp_load_l2 = map2(true_patterns, nmfp_loadings_re,
+                             function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA}),
+         nmfp_score_l2 = map2(true_scores, nmfp_scores_re,
+                              function(x,y) if(!any(is.na(y))) {norm(x-y, "F")/norm(x, "F")} else{NA})
+  )
+
+output_all <- output_all %>%
   dplyr::select(seed, participants, chemicals, patterns,
                 grep("rank", colnames(.)),
                 grep("_l2er", colnames(.)),
                 grep("ssdist", colnames(.)),
                 grep("score_l2", colnames(.)),
-                grep("load_l2", colnames(.))) %>% 
-  unnest(pca_rank:nmfp_load_l2)
+                grep("load_l2", colnames(.)))
 
 save(output_all, file = paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/combo_dim/dim_out_", job_num, ".RDA"))
-
-
