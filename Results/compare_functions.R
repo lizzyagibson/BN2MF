45c7e22612d92a728d6961d853097c4bac29177c
lizzyagibson <eag2186@cumc.columbia.edu>
2020-12-11 17:40:33 -0500

    Add error handling.

--- Results/compare_functions.R
+++ Results/compare_functions.R
@@ -1,7 +1,7 @@
 # Functions for BN2MF project
 
-# Functions to run other models
-# & to choose number of factors/components
+## Functions to run other models
+## & to choose number of factors/components
 
 # PCA
 get_pca <- function (sim) {
@@ -28,148 +28,22 @@ get_pca <- function (sim) {
   return(list(rotations = rotations, scores = scores, pred = pred, rank = rank))
 }
 
-# Factor Analysis
-get_fa <- function (sim) {
-  set.seed(1988)
-  fa_3 <- fa(sim, 3, scores = "regression", rotate = "promax")
-  fa_4 <- fa(sim, 4, scores = "regression", rotate = "promax")
-  fa_5 <- fa(sim, 5, scores = "regression", rotate = "promax")
-  
-  # Choose the model with the LOWEST BIC
-  if (min(fa_5$BIC, fa_4$BIC, fa_3$BIC) == fa_5$BIC) {
-    fa_out <- fa_5
-    rank <- 5
-  } else if (min(fa_5$BIC, fa_4$BIC, fa_3$BIC) == fa_4$BIC) {
-    fa_out <- fa_4
-    rank <- 4
-  } else {
-    fa_out <- fa_3
-    rank <- 3
-  }
-  
-  loadings <- matrix(fa_out$loadings, ncol = ncol(fa_out$scores))
-  fa_scores <- fa_out$scores
-  pred <- fa_scores %*% t(loadings)
-  return(list(loadings = loadings, fa_scores = fa_scores, pred = pred, rank = rank))
-}
-
-# NMF L2
-get_nmf_l2 <- function (sim) {
-  set.seed(1988)
-  nmf_3 <- nmf(sim, 3, nrun = 100, method = "lee")
-  nmf_4 <- nmf(sim, 4, nrun = 100, method = "lee")
-  nmf_5 <- nmf(sim, 5, nrun = 100, method = "lee")
-  
-  # Calculate BIC for each
-  bic_3 <- sum((sim - (basis(nmf_3)%*%coef(nmf_3)))^2) + 
-    (1/2)*(nrow(sim) + ncol(sim)) * 3 * log(nrow(sim) * ncol(sim))
-  bic_4 <- sum((sim - (basis(nmf_4)%*%coef(nmf_4)))^2) + 
-    (1/2)*(nrow(sim) + ncol(sim)) * 4 * log(nrow(sim) * ncol(sim))
-  bic_5 <- sum((sim - (basis(nmf_5)%*%coef(nmf_5)))^2) +  
-    (1/2)*(nrow(sim) + ncol(sim)) * 5 * log(nrow(sim) * ncol(sim))
-  
-  # Choose model with lowest BIC
-  if (min(bic_3, bic_4, bic_5) == bic_3) {
-    nmf_out <- nmf_3
-    rank <- 3
-  } else if (min(bic_3, bic_4, bic_5) == bic_4) {
-    nmf_out <- nmf_4
-    rank <- 4
-  } else {
-    nmf_out <- nmf_5
-    rank <- 5
-  }
-  
-  basis <- basis(nmf_out)
-  coef <- coef(nmf_out)
-  pred <- basis %*% coef
-  return(list(coef = coef, basis = basis, pred = pred, rank = rank))
-}
-
-# NMF Poisson
-get_nmf_p <- function (sim) {
-  set.seed(1988)
-  nmf_3 <- nmf(sim, 3, nrun = 100, method = "brunet")
-  nmf_4 <- nmf(sim, 4, nrun = 100, method = "brunet")
-  nmf_5 <- nmf(sim, 5, nrun = 100, method = "brunet")
-  
-  bic_3 <- -sum((sim * log(basis(nmf_3) %*% coef(nmf_3))) - (basis(nmf_3) %*% coef(nmf_3))) + 
-    (1/2)*(nrow(sim) + ncol(sim)) * 3 * log(nrow(sim) * ncol(sim))
-  bic_4 <- -sum((sim * log(basis(nmf_4) %*% coef(nmf_4))) - (basis(nmf_4) %*% coef(nmf_4))) + 
-    (1/2)*(nrow(sim) + ncol(sim)) * 4 * log(nrow(sim) * ncol(sim))
-  bic_5 <- -sum((sim * log(basis(nmf_5) %*% coef(nmf_5))) - (basis(nmf_5) %*% coef(nmf_5))) + 
-    (1/2)*(nrow(sim) + ncol(sim)) * 5 * log(nrow(sim) * ncol(sim))
-  
-  if (min(bic_3, bic_4, bic_5) == bic_3) {
-    nmf_out <- nmf_3
-    rank <- 3
-  } else if (min(bic_3, bic_4, bic_5) == bic_4) {
-    nmf_out <- nmf_4
-    rank <- 4
-  } else {
-    nmf_out <- nmf_5
-    rank <- 5
-  }
-  
-  basis <- basis(nmf_out)
-  coef <- coef(nmf_out)
-  pred <- basis %*% coef
-  return(list(coef = coef, basis = basis, pred = pred, rank = rank))
-}
-
-# Symmetric Subspace Distance 
-symm_subspace_dist <- function(U, V) {
-  
-  if (nrow(U) != max(nrow(U), ncol(U))) {U <- t(U)}
-  if (nrow(V) != max(nrow(V), ncol(V))) {V <- t(V)}
-  
-  qrU <- qr.Q(qr(U))
-  qrV <- qr.Q(qr(V))
-  
-  m <- ncol(U)
-  n <- ncol(V)
-  
-  dUV <- sqrt( max(m,n) - sum((t(qrU) %*% qrV)^2) )
-  
-  ratio <- dUV/sqrt( max(m,n))
-  
-  ratio
-  
-}
-
-# Cosine distance
-cos_dist <- function(a, b){
-  # a = as.matrix(a)
-  # b = as.matrix(b)
-  a = as.vector(a)
-  b = as.vector(b)
-
-  #cos_dist = c()
-  # for (i in 1:ncol(a)) {
-  #   cos_dist[i] = (a[,i] %*% b[,i]) / (norm(a[,i], "2")*norm(b[,i], "2"))
-  # }
-  cos_sim = (a %*% b) / (norm(a, "2")*norm(b, "2"))
-  return(1-cos_sim)
-}
-
-## For Simulations with Different Dimensions
-
 # Factor analysis
-get_fa_dim <- function (sim, patterns) {
+get_fa <- function (sim, patterns) {
   
   set.seed(1988)
   
   patternsm1 = ifelse(patterns == 1, 3, patterns - 1)
   patternsp1 = patterns+1
   
-  fa_3 <- try(fa(sim, patternsm1, scores = "regression", rotate = "promax"))
-  fa_4 <- try(fa(sim, patterns, scores = "regression", rotate = "promax"))
-  fa_5 <- try(fa(sim, patternsp1, scores = "regression", rotate = "promax"))
+  fa_3 <- try(fa(sim, patternsm1, scores = "regression", rotate = "promax", fm = "ml"))
+  fa_4 <- try(fa(sim, patterns,   scores = "regression", rotate = "promax", fm = "ml"))
+  fa_5 <- try(fa(sim, patternsp1, scores = "regression", rotate = "promax", fm = "ml"))
   
   # Choose the model with the LOWEST BIC
-  fa_3$BIC = if(all(class(fa_3) != "try-error")) {fa_3$BIC} else{NA}
-  fa_4$BIC = if(all(class(fa_4) != "try-error")) {fa_4$BIC} else{NA}
-  fa_5$BIC = if(all(class(fa_5) != "try-error")) {fa_5$BIC} else{NA}
+  if(all(class(fa_3) == "try-error")) {fa_3$BIC = NA}
+  if(all(class(fa_4) == "try-error")) {fa_4$BIC = NA}
+  if(all(class(fa_5) == "try-error")) {fa_5$BIC = NA}
   
   if (min(fa_5$BIC, fa_4$BIC, fa_3$BIC, na.rm = T) == fa_5$BIC & !is.na(fa_5$BIC)) {
     fa_out <- fa_5
@@ -188,24 +62,28 @@ get_fa_dim <- function (sim, patterns) {
   return(list(loadings = loadings, fa_scores = fa_scores, pred = pred, rank = rank))
 }
 
+# Calculate BIC
+get_bic <- function(sim, patterns, scores, loadings){
+  bic = sum((sim - (scores%*%loadings))^2) + 
+    (1/2)*(nrow(sim) + ncol(sim)) * patterns * log(nrow(sim) * ncol(sim))
+  return(bic)
+}
+
 # NMF L2
-get_nmf_l2_dim <- function (sim, patterns) {
+get_nmf_l2 <- function (sim, patterns) {
   
   patternsm1 = ifelse(patterns == 1, 3, patterns - 1)
   patternsp1 = patterns+1
   
   set.seed(1988)
   nmf_3 <- nmf(sim, patternsm1, nrun = 100, method = "lee")
-  nmf_4 <- nmf(sim, patterns, nrun = 100, method = "lee")
+  nmf_4 <- nmf(sim, patterns,   nrun = 100, method = "lee")
   nmf_5 <- nmf(sim, patternsp1, nrun = 100, method = "lee")
   
   # Calculate BIC for each
-  bic_3 <- sum((sim - (basis(nmf_3)%*%coef(nmf_3)))^2) + 
-    (1/2)*(nrow(sim) + ncol(sim)) * patternsm1 * log(nrow(sim) * ncol(sim))
-  bic_4 <- sum((sim - (basis(nmf_4)%*%coef(nmf_4)))^2) + 
-    (1/2)*(nrow(sim) + ncol(sim)) * patterns * log(nrow(sim) * ncol(sim))
-  bic_5 <- sum((sim - (basis(nmf_5)%*%coef(nmf_5)))^2) +  
-    (1/2)*(nrow(sim) + ncol(sim)) * patternsp1 * log(nrow(sim) * ncol(sim))
+  bic_3 <- get_bic(sim, patternsm1, basis(nmf_3), coef(nmf_3))
+  bic_4 <- get_bic(sim, patterns,   basis(nmf_4), coef(nmf_4))
+  bic_5 <- get_bic(sim, patternsp1, basis(nmf_5), coef(nmf_5))
   
   # Choose model with lowest BIC
   if (min(bic_3, bic_4, bic_5) == bic_3) {
@@ -226,22 +104,20 @@ get_nmf_l2_dim <- function (sim, patterns) {
 }
 
 # NMF Poisson
-get_nmf_p_dim <- function (sim, patterns) {
+get_nmf_p <- function (sim, patterns) {
   
   patternsm1 = ifelse(patterns == 1, 3, patterns - 1)
   patternsp1 = patterns+1
   
   set.seed(1988)
   nmf_3 <- nmf(sim, patternsm1, nrun = 100, method = "brunet")
-  nmf_4 <- nmf(sim, patterns, nrun = 100, method = "brunet")
+  nmf_4 <- nmf(sim, patterns,   nrun = 100, method = "brunet")
   nmf_5 <- nmf(sim, patternsp1, nrun = 100, method = "brunet")
   
-  bic_3 <- -sum((sim * log(basis(nmf_3) %*% coef(nmf_3))) - (basis(nmf_3) %*% coef(nmf_3))) + 
-    (1/2)*(nrow(sim) + ncol(sim)) * patternsm1 * log(nrow(sim) * ncol(sim))
-  bic_4 <- -sum((sim * log(basis(nmf_4) %*% coef(nmf_4))) - (basis(nmf_4) %*% coef(nmf_4))) + 
-    (1/2)*(nrow(sim) + ncol(sim)) * patterns * log(nrow(sim) * ncol(sim))
-  bic_5 <- -sum((sim * log(basis(nmf_5) %*% coef(nmf_5))) - (basis(nmf_5) %*% coef(nmf_5))) + 
-    (1/2)*(nrow(sim) + ncol(sim)) * patternsp1 * log(nrow(sim) * ncol(sim))
+  # Calculate BIC for each
+  bic_3 <- get_bic(sim, patternsm1, basis(nmf_3), coef(nmf_3))
+  bic_4 <- get_bic(sim, patterns,   basis(nmf_4), coef(nmf_4))
+  bic_5 <- get_bic(sim, patternsp1, basis(nmf_5), coef(nmf_5))
   
   if (min(bic_3, bic_4, bic_5) == bic_3) {
     nmf_out <- nmf_3
@@ -259,3 +135,133 @@ get_nmf_p_dim <- function (sim, patterns) {
   pred <- basis %*% coef
   return(list(coef = coef, basis = basis, pred = pred, rank = rank))
 }
+
+## Metric Functions
+## to compare performance
+
+# Symmetric Subspace Distance 
+symm_subspace_dist <- function(U, V) {
+  
+  # patterns should be columns
+  if (nrow(U) < ncol(U)) {U <- t(U)}
+  if (nrow(V) < ncol(V)) {V <- t(V)}
+  
+  qrU <- qr.Q(qr(U))
+  qrV <- qr.Q(qr(V))
+  
+  m <- ncol(U)
+  n <- ncol(V)
+  
+  dUV <- sqrt( max(m,n) - sum((t(qrU) %*% qrV)^2) )
+  
+  ratio <- dUV/sqrt( max(m,n))
+  
+  ratio
+  
+}
+
+# Cosine distance (matrix version)
+cos_dist <- function(a, b){
+
+  if(any(is.na(a)) | any(is.na(b))) {return(NA)} else{
+    
+    # patterns should be columns
+    if (nrow(a) < ncol(a)) {a <- t(a)}
+    if (nrow(b) < ncol(b)) {b <- t(b)}
+  
+    if(ncol(a) != ncol(b)) { return(NA) } else{
+        a = as.vector(a)
+        b = as.vector(b)
+        cos_sim = (a %*% b) / (norm(a, "2")*norm(b, "2"))
+        return(1-cos_sim) 
+    }
+  }
+}
+
+# Cosine distance (vector version)
+cos_dist_v <- function(a, b){
+  
+  if(any(is.na(a)) | any(is.na(b))) {return(NA)} else{
+    
+       a = as.matrix(a)
+       b = as.matrix(b)
+
+       # patterns should be columns
+       if (nrow(a) < ncol(a)) {a <- t(a)}
+       if (nrow(b) < ncol(b)) {b <- t(b)}
+       
+       if(ncol(a) != ncol(b)) { return(NA) } else{
+         cos_sim = c()
+         for (i in 1:ncol(a)) {
+          cos_sim[i] = (a[,i] %*% b[,i]) / (norm(a[,i], "2")*norm(b[,i], "2"))
+          }
+         return(1-cos_sim) 
+       }
+  }
+  }
+
+# L2 relative error
+# WITH error handling
+# if the matrices are not the same size, NA
+get_relerror <- function(x,y) {
+
+  if(any(is.na(x)) | any(is.na(y))) {return(NA)} else{
+    
+        x = as.matrix(x)
+        y = as.matrix(y)
+        
+        # patterns should be columns
+        if (nrow(x) < ncol(x)) {x <- t(x)}
+        if (nrow(y) < ncol(y)) {y <- t(y)}  
+    
+        if(ncol(x) != ncol(y)) { return(NA) } else{
+                return(norm(x-y, "F")/norm(x, "F"))
+              }
+  }
+  }
+  
+# Factor correspondence
+# WITH error handling
+# if matrices are not same size, NA
+# If nn is false, we allow *signed permutations*
+get_perm <- function(x,y, nn = TRUE) {
+  
+  if(any(is.na(x)) | any(is.na(y))) {return(NA)} else{
+    
+      x = as.matrix(x)
+      y = as.matrix(y)
+      
+      # patterns should be columns
+      if (nrow(x) < ncol(x)) {x <- t(x)}
+      if (nrow(y) < ncol(y)) {y <- t(y)}  
+      
+      if(ncol(x) != ncol(y)) { return(NA) } else{
+        return(factor_correspondence(x, y, nn)$permutation_matrix) } 
+      }
+}
+
+get_product <- function(x,y) {
+  if(any(is.na(x)) | any(is.na(y))) {return(NA)} else{
+    
+    x = as.matrix(x)
+    y = as.matrix(y)
+    
+    # patterns should be columns
+    if (nrow(x) < ncol(x)) {x <- t(x)}
+    
+    if(ncol(x) != ncol(y)) { return(NA) } else{ return(x %*% y) }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
