8365e6afa0b02d8f674f2939ad0a5dff5b8ab624
lizzyagibson <eag2186@cumc.columbia.edu>
2021-01-23 15:45:05 -0500

    Combine bootstraps locally.

--- Bootstrap/bootstrap_combo.R
+++ Bootstrap/bootstrap_combo.R
@@ -17,7 +17,7 @@ bs_list_eh  = list(rep(NA, datasets))
 # 100 bootstrapped results per array
 for (run in 1:datasets) {
   bs_list_ewa[[run]] <- array(dim = c(1000, 4, bootstraps))
-  bs_list_eh[[run]]  <- array(dim = c(1000, 4, bootstraps))
+  bs_list_eh[[run]]  <- array(dim = c(4, 50, bootstraps))
 }
 
 for (run in 1:datasets) {
@@ -25,34 +25,36 @@ for (run in 1:datasets) {
   for (boot in 1:bootstraps) {
     if (file.exists(paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/bootstrap_cor_ewa/cor_ewa_bs_", 
                                    boot, "sim_", sim_num, ".mat"))) {
-    bs_list_ewa[[run]] = readMat(paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/bootstrap_cor_ewa/cor_ewa_bs_", 
+    bs_list_ewa[[run]][,,boot] = readMat(paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/bootstrap_cor_ewa/cor_ewa_bs_", 
                                         boot, "sim_", sim_num, ".mat"))[[1]]
-    bs_list_eh[[run]]  = readMat(paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/bootstrap_cor_eh/cor_eh_bs_",  
+    bs_list_eh[[run]][,,boot]  = readMat(paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/bootstrap_cor_eh/cor_eh_bs_",  
                                         boot, "sim_", sim_num, ".mat"))[[1]]
     }
-    print(paste0("Bootstrap Number: ", boot))
-  }
+    }
   print(paste0("Run Number: ", run))
+  str(bs_list_ewa[[run]])
+  str(bs_list_eh[[run]])
   }
 
 # Create empirical confidence interval
 # List of matrices
 # One matrix for each BN2MF output
-# 25th, mean, and 75th percentile matrices (3 lists)
+# 25th, mean, and 75th percentile matrices (3 lists) for EWA and mean EH
 
 bs_list_lower = list(rep(NA, datasets))
 bs_list_upper = list(rep(NA, datasets))
 bs_list_mean  = list(rep(NA, datasets))
+bs_list_mean_h  = list(rep(NA, datasets))
 
 for (run in 1:datasets) {
   bs_list_lower[[run]] <- apply(bs_list_ewa[[run]], c(1,2), quantile, 0.025, na.rm = TRUE)
   bs_list_upper[[run]] <- apply(bs_list_ewa[[run]], c(1,2), quantile, 0.975, na.rm = TRUE)
   bs_list_mean[[run]]  <- apply(bs_list_ewa[[run]], c(1,2), mean, na.rm = TRUE)
+  bs_list_mean_h[[run]]    <- apply(bs_list_eh[[run]], c(1,2), mean, na.rm = TRUE)
 }
 
 save(bs_list_lower, file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/bootstrap_cor/bs_list_lower.RDA")
 save(bs_list_upper, file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/bootstrap_cor/bs_list_upper.RDA")
 save(bs_list_mean,  file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/bootstrap_cor/bs_list_mean.RDA")
-
-
+save(bs_list_mean_h,    file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/bootstrap_cor/bs_list_eh.RDA")
 
--- Bootstrap/bootstrap_out.R
+++ Bootstrap/bootstrap_out.R
@@ -1,81 +1,132 @@
-  library(tidyverse)
-  library(R.matlab)
-  library(openssl)
-  library(plotly)
+#####  Load packages
+library(tidyverse)
+library(R.matlab)
+library(openssl)
+library(plotly)
+
+# create random vector for matlab on hpc
+rand_vec = rand_num(1000) * ((2^31)-1)
+
+#####   
+##### Single Sim Example
+#####
+
+# Read data
+chem <- read_csv("./Results/Main/Corr Ex/corr_chem.csv")
+sim <- read_csv("./Results/Main/Corr Ex/corr_sim.csv")
+patterns <- read_csv("./Results/Main/Corr Ex/corr_patterns.csv")
+scores <- read_csv("./Results/Main/Corr Ex/corr_scores.csv")
+
+v_EWA <- readMat("/Users/lizzy/BN2MF/Bootstrap/compare/cor_EWA.mat")[[1]]
+v_EH <- readMat("/Users/lizzy/BN2MF/Bootstrap/compare/cor_EH.mat")[[1]]
+v_upper <- readMat("/Users/lizzy/BN2MF/Bootstrap/compare/cor_WA_upper.mat")[[1]]
+v_lower <- readMat("/Users/lizzy/BN2MF/Bootstrap/compare/cor_WA_lower.mat")[[1]]
+v_dist <- readMat("/Users/lizzy/BN2MF/Bootstrap/compare/cor_var_dist_WA.mat")[[1]]
+
+# Normalize truth
+patterns_denom      = apply(patterns, 1, sum)
+patterns_scaled     = patterns/patterns_denom
+patterns_denom_diag = diag(patterns_denom)
+scores_scaled       = as.matrix(scores) %*% patterns_denom_diag;
+
+# Read bootstrapped BN2MF EWA output
+bs_dist <- array(dim = c(1000, 4, 500))
+
+for (i in 1:500) {
+  bs_dist[,,i] <- readMat(paste0("/Users/lizzy/BN2MF/Bootstrap/bootstrap_cor/bs_ewa_", i, ".mat"))[[1]]
+  }
+
+head(bs_dist[,,14])
+
+# Create empirical confidence interval
+bs_EWA   <- apply(bs_dist, c(1,2), mean, na.rm = TRUE)
+bs_lower <- apply(bs_dist, c(1,2), quantile, 0.025, na.rm = TRUE)
+bs_upper <- apply(bs_dist, c(1,2), quantile, 0.975, na.rm = TRUE)
+
+## Example viz
+truth = scores_scaled[1,1]
+
+v_ewa = v_EWA[1,1]
+v_dist_1 = v_dist[1,1,]
+v_q25 = v_lower[1,1]
+v_q75 = v_upper[1,1]
+
+bs_ewa = bs_EWA[1,1]
+bs_dist_1 = bs_dist[1,1,]
+bs_q25 = bs_lower[1,1]
+bs_q75 = bs_upper[1,1]
+
+histo <- tibble(Distribution = v_dist_1) %>% 
+  mutate(Type = "Variational") %>% 
+  rbind(., tibble(Distribution = bs_dist_1) %>% 
+          mutate(Type = "Bootstrap")) %>% 
+  ggplot(aes(x = Distribution)) +
+  geom_rect(aes(xmin=bs_q25, xmax=bs_q75, ymin=0, ymax=Inf), fill="pink",      alpha=0.05) +
+  geom_rect(aes(xmin=v_q25,  xmax=v_q75,  ymin=0, ymax=Inf), fill="lightblue", alpha=0.05) +
+  geom_histogram(aes(y=..density.., group = Type, fill = Type), alpha = 0.5, bins = 100) +
+  scale_fill_manual(values = c("red", "blue")) +
+  theme_bw() + 
+  geom_vline(xintercept = truth,            linetype="dotted", color = "black") +
+  geom_vline(xintercept = c(v_ewa, bs_ewa), linetype="dotted", color = "yellow") + 
+  xlim(0, 450) + ylab("Density")
+
+histo
+
+sum(scores_scaled <= v_upper & scores_scaled >= v_lower)/4000
+sum(scores_scaled <= bs_upper & scores_scaled >= bs_lower)/4000
   
-  # create random vector for matlab on hpc
-  rand_vec = rand_num(1000) * ((2^31)-1)
-   
-  # Read data
-  chem <- read_csv("./Results/Main/Corr Ex/corr_chem.csv")
-  sim <- read_csv("./Results/Main/Corr Ex/corr_sim.csv")
-  patterns <- read_csv("./Results/Main/Corr Ex/corr_patterns.csv")
-  scores <- read_csv("./Results/Main/Corr Ex/corr_scores.csv")
-  
-  v_EWA <- readMat("/Users/lizzy/BN2MF/Bootstrap/compare/cor_EWA.mat")[[1]]
-  v_EH <- readMat("/Users/lizzy/BN2MF/Bootstrap/compare/cor_EH.mat")[[1]]
-  v_upper <- readMat("/Users/lizzy/BN2MF/Bootstrap/compare/cor_WA_upper.mat")[[1]]
-  v_lower <- readMat("/Users/lizzy/BN2MF/Bootstrap/compare/cor_WA_lower.mat")[[1]]
-  v_dist <- readMat("/Users/lizzy/BN2MF/Bootstrap/compare/cor_var_dist_WA.mat")[[1]]
-  
-  # Normalize truth
-  patterns_denom      = apply(patterns, 1, sum)
-  patterns_scaled     = patterns/patterns_denom
-  patterns_denom_diag = diag(patterns_denom)
-  scores_scaled       = as.matrix(scores) %*% patterns_denom_diag;
-  
-  # Read bootstrapped BN2MF EWA output
-  bs_dist <- array(dim = c(1000, 4, 500))
-  
-  for (i in 1:500) {
-    bs_dist[,,i] <- readMat(paste0("/Users/lizzy/BN2MF/Bootstrap/bootstrap_cor/bs_ewa_", i, ".mat"))[[1]]
-    }
-  
-  head(bs_dist[,,14])
-  
-  # Create empirical confidence interval
-  
-  bs_EWA   <- apply(bs_dist, c(1,2), mean, na.rm = TRUE)
-  bs_lower <- apply(bs_dist, c(1,2), quantile, 0.025, na.rm = TRUE)
-  bs_upper <- apply(bs_dist, c(1,2), quantile, 0.975, na.rm = TRUE)
-  
-  head(bs_lower)
-  head(bs_EWA)
-  head(bs_upper)
-  head(scores_scaled)
-  head(v_EWA)
-  
-  ## Example viz
-  truth = scores_scaled[1,1]
-  
-  v_ewa = v_EWA[1,1]
-  v_dist_1 = v_dist[1,1,]
-  v_q25 = v_lower[1,1]
-  v_q75 = v_upper[1,1]
-  
-  bs_ewa = bs_EWA[1,1]
-  bs_dist_1 = bs_dist[1,1,]
-  bs_q25 = bs_lower[1,1]
-  bs_q75 = bs_upper[1,1]
-  
-  histo <- tibble(Distribution = v_dist_1) %>% 
-    mutate(Type = "Variational") %>% 
-    rbind(., tibble(Distribution = bs_dist_1) %>% 
-            mutate(Type = "Bootstrap")) %>% 
-    ggplot(aes(x = Distribution)) +
-    geom_rect(aes(xmin=bs_q25, xmax=bs_q75, ymin=0, ymax=Inf), fill="pink",      alpha=0.05) +
-    geom_rect(aes(xmin=v_q25,  xmax=v_q75,  ymin=0, ymax=Inf), fill="lightblue", alpha=0.05) +
-    geom_histogram(aes(y=..density.., group = Type, fill = Type), alpha = 0.5, bins = 100) +
-    scale_fill_manual(values = c("red", "blue")) +
-    theme_bw() + 
-    geom_vline(xintercept = truth,            linetype="dotted", color = "black") +
-    geom_vline(xintercept = c(v_ewa, bs_ewa), linetype="dotted", color = "yellow") + 
-    xlim(0, 450) + ylab("Density")
-  
-  histo
-  
-  sum(scores_scaled <= v_upper & scores_scaled >= v_lower)/4000
-  sum(scores_scaled <= bs_upper & scores_scaled >= bs_lower)/4000
-  
+#####   
+##### 100 Correlated Sims
+#####
+
+# Read data
+load("./Bootstrap/bs_list_lower.RDA")
+load("./Bootstrap/bs_list_upper.RDA")
+load("./Bootstrap/bs_list_mean.RDA")
+
+bs_dat = tibble(seed = 201:300,
+              lower = bs_list_lower,
+              mean = bs_list_mean,
+              upper = bs_list_upper) %>% 
+         mutate()
+
+dim(bs_dat$lower[[1]])
+
+sum(scores_scaled <= bs_dat$upper[[2]] & scores_scaled >= bs_dat$lower[[2]])/4000
+
+head(bs_dat$lower[[1]])
+head(bs_dat$mean[[1]])
+head(bs_dat$upper[[1]])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   
