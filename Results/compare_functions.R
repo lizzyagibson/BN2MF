b4710e25ece047546d597dc6022ad9f518481eb2
lizzyagibson <eag2186@cumc.columbia.edu>
2021-01-31 16:20:32 -0500

    Add conditional to load libraries.

--- Results/compare_functions.R
+++ Results/compare_functions.R
@@ -4,13 +4,23 @@
 ## & to choose number of factors/components
 
 # Packages
-library(tidyverse, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
-library(R.matlab, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
-library(psych, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
-library(rngtools, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
-library(registry, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
-library(pkgmaker, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
-library(NMF, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+if (getwd() == "/Users/lizzy/BN2MF") {
+  library(tidyverse)
+  library(R.matlab)
+  library(psych)
+  #library(NMF)
+  library(CVXR)
+} else {
+  library(tidyverse, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+  library(R.matlab, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+  library(psych, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+  library(rngtools, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+  library(registry, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+  library(pkgmaker, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+  library(NMF, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+  library(CVXR, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+  library(GPArotation, lib.loc = "/ifs/home/msph/ehs/eag2186/local/hpc/")
+}
 
 # PCA
 get_pca <- function (sim) {
@@ -151,28 +161,37 @@ get_nmfp <- function (sim, patterns) {
 # Symmetric Subspace Distance 
 symm_subspace_dist <- function(U, V) {
   
-  # patterns should be columns
-  if (nrow(U) < ncol(U)) {U <- t(U)}
-  if (nrow(V) < ncol(V)) {V <- t(V)}
+  if(any(is.na(U)) | any(is.na(V))) {return(NA)} else{
+    
+    U = as.matrix(U)
+    V = as.matrix(V)
+    
+    # patterns should be columns
+    if (nrow(U) < ncol(U)) {U <- t(U)}
+    if (nrow(V) < ncol(V)) {V <- t(V)}
+    
+    if(ncol(U) != ncol(V)) { return(NA) } else{
+    qrU <- qr.Q(qr(U))
+    qrV <- qr.Q(qr(V))
   
-  qrU <- qr.Q(qr(U))
-  qrV <- qr.Q(qr(V))
+    m <- ncol(U)
+    n <- ncol(V)
   
-  m <- ncol(U)
-  n <- ncol(V)
+    dUV <- sqrt( max(m,n) - sum((t(qrU) %*% qrV)^2) )
   
-  dUV <- sqrt( max(m,n) - sum((t(qrU) %*% qrV)^2) )
-  
-  ratio <- dUV/sqrt( max(m,n))
-  
-  ratio
-  
+    ratio <- dUV/sqrt( max(m,n))
+    return(ratio)
+    }
+  }
 }
 
 # Cosine distance (matrix version)
 cos_dist <- function(a, b){
 
   if(any(is.na(a)) | any(is.na(b))) {return(NA)} else{
+    
+    a = as.matrix(a)
+    b = as.matrix(b)
     
     # patterns should be columns
     if (nrow(a) < ncol(a)) {a <- t(a)}
@@ -182,7 +201,7 @@ cos_dist <- function(a, b){
         a = as.vector(a)
         b = as.vector(b)
         cos_sim = (a %*% b) / (norm(a, "2")*norm(b, "2"))
-        return(1-cos_sim) 
+        return(as.numeric(1-cos_sim)) 
     }
   }
 }
@@ -233,6 +252,53 @@ get_relerror <- function(x,y) {
 # WITH error handling
 # if matrices are not same size, NA
 # If nn is false, we allow *signed permutations*
+factor_correspondence <- function (A, B, nn = TRUE) {
+  
+  G <- t(B) %*% A
+  n <- nrow(G)
+  
+  # Step 1. Define the variable to be estimated
+  # Pi -- the permutation or signed permutation matrix
+  Pi <- Variable(n,n)
+  
+  # Step 2. Define the objective to be optimized
+  objective <- Maximize(base::sum(Pi * G))
+  
+  if (nn) {
+    # Step 2.5. Subject to these constraints
+    constX = list()
+    for (i in 1:nrow(G)) {
+      constX <-  c(constX, list(base::sum(Pi[,i]) == 1))
+      constX <-  c(constX, list(base::sum(Pi[i,]) == 1))
+    }
+    constX <- c(constX, Pi >= 0)
+  } else {
+    # % allow sign flips 
+    # Step 3. vector l1 norms along rows and columns
+    constX = list()
+    for (i in 1:nrow(G)) {
+      constX <-  c(constX, list(base::sum(abs(Pi[,i])) <= 1))
+      constX <-  c(constX, list(base::sum(abs(Pi[i,])) <= 1))
+    }
+  }  
+  # Step 3. Create a problem to solve
+  problem <- Problem(objective, constraints = constX)
+  
+  # Step 4. Solve it!
+  result <- solve(problem)
+  
+  # Step 5. Extract solution and objective value
+  perm <- round(result$getValue(Pi), 0)
+  
+  e <- norm(B,'f')^2 + norm(A,'f')^2 - 2 * base::sum(perm * G)
+  # e -- the sum of squared errors under the best calibration \Pi
+  
+  # New matrix with best order
+  newB <- B %*% perm
+  
+  return(list(rearranged = newB, permutation_matrix = perm))
+}
+
 get_perm <- function(x,y, nn = TRUE) {
   
   if(any(is.na(x)) | any(is.na(y))) {return(NA)} else{
