6fd550e06b360dd040e0025eaa3c18e043e25047
lizzyagibson <eag2186@cumc.columbia.edu>
2021-02-02 09:36:39 -0500

    Add predicted value bootstraps.

--- Bootstrap/bootstrap_combo.R
+++ Bootstrap/bootstrap_combo.R
@@ -13,33 +13,36 @@ bootstraps = 150
 datasets = 300
 
 # List with one entry per simulation
-bs_list_ewa = list()
-bs_list_eh  = list()
+bs_list_ewa  = list()
+bs_list_eh   = list()
+bs_list_pred = list()
 
 # One array for each BN2MF output
-for (run in 1:datasets) {
-  bs_list_ewa[[run]] <- array(dim = c(1000, 5, bootstraps)) # 5 includes row ID
-  bs_list_eh[[run]]  <- array(dim = c(4, 50, bootstraps))
+for (set in 1:datasets) {
+  bs_list_ewa[[set]] <- array(dim = c(1000, 5, bootstraps)) # 5 includes row ID
+  bs_list_eh[[set]]  <- array(dim = c(4, 50, bootstraps))
+  bs_list_pred[[set]] <- array(dim = c(1000, 50, bootstraps))
 }
 
 # Read in data output by `bootstrap_bn2mf.m`
-for (run in 1:datasets) {
+for (set in 1:datasets) {
   
   type = ""
-  if (run <= 100) {type = "dist"
-  } else if (run <= 200 & run > 100) {
-    type = "over"} else if (run > 200) {type = "cor"}
+  if (set <= 100) {type = "dist"
+  } else if (set <= 200 & set > 100) {
+    type = "over"} else if (set > 200) {type = "cor"}
   
   for (boot in 1:bootstraps) {
     if (file.exists(paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_", type, "_ewa/", 
-                           type, "_ewa_bs_", boot, "_sim_", run, ".mat"))) {
-    bs_list_ewa[[run]][,,boot] = readMat(paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_", type, "_ewa/", 
-                                                type, "_ewa_bs_", boot, "_sim_", run, ".mat"))[[1]]
-    bs_list_eh[[run]][,,boot]  = readMat(paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_", type, "_eh/", 
-                                                type, "_eh_bs_", boot, "_sim_", run, ".mat"))[[1]]
+                           type, "_ewa_bs_", boot, "_sim_", set, ".mat"))) {
+    bs_list_ewa[[set]][,,boot] = readMat(paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_", type, "_ewa/", 
+                                                type, "_ewa_bs_", boot, "_sim_", set, ".mat"))[[1]]
+    bs_list_eh[[set]][,,boot]  = readMat(paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_", type, "_eh/", 
+                                                type, "_eh_bs_", boot, "_sim_", set, ".mat"))[[1]]
+    bs_list_pred[[set]][,,boot] <- bs_list_ewa[[set]][,2:5,boot] %*% bs_list_eh[[set]][,,boot]
     }
     }
-  print(paste0("Run Number: ", run))
+  print(paste0("Reading Dataset Number: ", set))
   }
 
 # EH
@@ -51,11 +54,12 @@ bs_list_upper_h  = list()
 bs_list_mean_h   = list()
 bs_list_median_h = list()
 
-for (run in 1:datasets) {
-  bs_list_lower_h[[run]]  <- apply(bs_list_eh[[run]], c(1,2), quantile, 0.025, na.rm = TRUE)
-  bs_list_upper_h[[run]]  <- apply(bs_list_eh[[run]], c(1,2), quantile, 0.975, na.rm = TRUE)  
-  bs_list_mean_h[[run]]   <- apply(bs_list_eh[[run]], c(1,2), mean, na.rm = TRUE)
-  bs_list_median_h[[run]] <- apply(bs_list_eh[[run]], c(1,2), median, na.rm = TRUE)
+for (set in 1:datasets) {
+  bs_list_lower_h[[set]]  <- apply(bs_list_eh[[set]], c(1,2), quantile, 0.025, na.rm = TRUE)
+  bs_list_upper_h[[set]]  <- apply(bs_list_eh[[set]], c(1,2), quantile, 0.975, na.rm = TRUE)  
+  bs_list_mean_h[[set]]   <- apply(bs_list_eh[[set]], c(1,2), mean, na.rm = TRUE)
+  bs_list_median_h[[set]] <- apply(bs_list_eh[[set]], c(1,2), median, na.rm = TRUE)
+  print(paste0("EH Summary Statistics for Dataset Number: ", set))
 }
 
 # all `bs_list...` files go into `bootstrap_out.R`
@@ -63,6 +67,7 @@ save(bs_list_lower_h,  file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/boot
 save(bs_list_upper_h,  file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_lists/bs_list_upper_h.RDA")
 save(bs_list_mean_h,   file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_lists/bs_list_mean_h.RDA")
 save(bs_list_median_h, file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_lists/bs_list_median_h.RDA")
+rm(list = c("bs_list_lower_h", "bs_list_upper_h", "bs_list_mean_h", "bs_list_median_h"))
 
 # Also save whole distribution for three examples
 save_dist = c(2, 102, 202)
@@ -72,17 +77,18 @@ for (i in 1:length(save_dist)) {
   save(save_eh, file = paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_lists/bs_", save_dist[i], "_eh_array.RDA"))
 }
 # this also goes into `bootstrap_out.R`
+rm(bs_list_eh)
 
 # EWA
 # first match permutations !!!
 # reorder IDs
-for (run in 1:datasets) {
+for (set in 1:datasets) {
   for (boot in 1:bootstraps) {
   # takes the first instance of an ID
   # all instances of same ID are equal
-    bs_list_ewa[[run]][,,boot] = bs_list_ewa[[run]][,,boot][match(1:1000, bs_list_ewa[[run]][,,boot][,1]),]
+    bs_list_ewa[[set]][,,boot] = bs_list_ewa[[set]][,,boot][match(1:1000, bs_list_ewa[[set]][,,boot][,1]),]
   }
-    print(paste0("Run Number: ", run))
+    print(paste0("Match Permutations for Dataset Number: ", set))
 }
 
 # Create empirical confidence interval
@@ -94,18 +100,20 @@ bs_list_upper_wa  = list()
 bs_list_mean_wa   = list()
 bs_list_median_wa = list()
 
-for (run in 1:datasets) {
-  bs_list_lower_wa[[run]]  <- apply(bs_list_ewa[[run]], c(1,2), quantile, 0.025, na.rm = TRUE)
-  bs_list_upper_wa[[run]]  <- apply(bs_list_ewa[[run]], c(1,2), quantile, 0.975, na.rm = TRUE)
-  bs_list_mean_wa[[run]]   <- apply(bs_list_ewa[[run]], c(1,2), mean,            na.rm = TRUE)
-  bs_list_median_wa[[run]] <- apply(bs_list_ewa[[run]], c(1,2), median,          na.rm = TRUE)
-}
+for (set in 1:datasets) {
+  bs_list_lower_wa[[set]]  <- apply(bs_list_ewa[[set]], c(1,2), quantile, 0.025, na.rm = TRUE)
+  bs_list_upper_wa[[set]]  <- apply(bs_list_ewa[[set]], c(1,2), quantile, 0.975, na.rm = TRUE)
+  bs_list_mean_wa[[set]]   <- apply(bs_list_ewa[[set]], c(1,2), mean,            na.rm = TRUE)
+  bs_list_median_wa[[set]] <- apply(bs_list_ewa[[set]], c(1,2), median,          na.rm = TRUE)
+  print(paste0("EWA Summary Statistics for Dataset Number: ", set))
+  }
 
 save(bs_list_lower_wa,  file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_lists/bs_list_lower_wa.RDA")
 save(bs_list_upper_wa,  file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_lists/bs_list_upper_wa.RDA")
 save(bs_list_mean_wa,   file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_lists/bs_list_mean_wa.RDA")
 save(bs_list_median_wa, file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_lists/bs_list_median_wa.RDA")
 # all `bs_list...` files go into `bootstrap_out.R`
+rm(list = c("bs_list_lower_wa", "bs_list_upper_wa", "bs_list_mean_wa", "bs_list_median_wa"))
 
 # Also save whole distribution for three examples
 save_dist = c(2, 102, 202)
@@ -116,3 +124,27 @@ for (i in 1:length(save_dist)) {
   save(save_ewa,  file = paste0("/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_lists/bs_", save_dist[i], "_ewa_array.RDA"))
 }
 # this also goes into `bootstrap_out.R`
+rm(bs_list_ewa)
+
+# Get predicted values
+# List of matrices
+# One matrix for each BN2MF output
+# 25th, mean, median, and 75th percentile matrices (4 lists) for predicted values
+bs_list_lower_pred  = list()
+bs_list_upper_pred  = list()
+bs_list_mean_pred   = list()
+bs_list_median_pred = list()
+
+for (set in 1:datasets) {
+  bs_list_lower_pred[[set]]  <- apply(bs_list_pred[[set]], c(1,2), quantile, 0.025, na.rm = TRUE)
+  bs_list_upper_pred[[set]]  <- apply(bs_list_pred[[set]], c(1,2), quantile, 0.975, na.rm = TRUE)
+  bs_list_mean_pred[[set]]   <- apply(bs_list_pred[[set]], c(1,2), mean,            na.rm = TRUE)
+  bs_list_median_pred[[set]] <- apply(bs_list_pred[[set]], c(1,2), median,          na.rm = TRUE)
+  print(paste0("Predicted Value Summary Statistics for Dataset Number: ", set))
+}
+
+save(bs_list_lower_pred,  file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_lists/bs_list_lower_pred.RDA")
+save(bs_list_upper_pred,  file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_lists/bs_list_upper_pred.RDA")
+save(bs_list_mean_pred,   file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_lists/bs_list_mean_pred.RDA")
+save(bs_list_median_pred, file = "/ifs/scratch/msph/ehs/eag2186/npbnmf/main/bs/bootstrap_lists/bs_list_median_pred.RDA")
+# all `bs_list...` files go into `bootstrap_out.R`
--- Bootstrap/bootstrap_out.R
+++ Bootstrap/bootstrap_out.R
@@ -6,7 +6,7 @@
 ####  Load packages ####
 library(tidyverse)
 library(R.matlab)
-library(openssl)
+#library(openssl)
 
 # create random vector for matlab on hpc
 # rand_vec = rand_num(150) * ((2^31)-1)
@@ -267,8 +267,8 @@ plot_h %>% dplyr::select(sim_type, bs_eh:truth) %>%
 plot_h %>% 
   mutate(sim_type = fct_inorder(sim_type)) %>% 
   ggplot(aes(x = Distribution, fill = Type)) +
-  geom_rect(aes(xmin = v_h_25,  xmax = v_h_75,  ymin=0, ymax=Inf), fill="lightblue", alpha=0.025) +
   geom_rect(aes(xmin = bs_h_25, xmax = bs_h_75, ymin=0, ymax=Inf), fill="pink",      alpha=0.025) +
+  geom_rect(aes(xmin = v_h_25,  xmax = v_h_75,  ymin=0, ymax=Inf), fill="lightblue", alpha=0.025) +
   geom_histogram(aes(y=..density..,fill = Type), bins = 100) +
   #geom_density(aes(group = Type), alpha = 0.5) +
   scale_fill_manual(values = c("red", "blue")) +
@@ -286,3 +286,231 @@ plot_h %>%
 
   
   
+
+#### Distribution on  predicted values ####
+load("./Bootstrap/compare/bs_list_lower_pred.RDA")
+load("./Bootstrap/compare/bs_list_upper_pred.RDA")
+load("./Bootstrap/compare/bs_list_mean_pred.RDA")
+load("./Bootstrap/compare/bs_list_median_pred.RDA")
+
+bs_pred = tibble(bs_pred_lower  = bs_list_lower_pred,
+                 bs_pred_mean   = bs_list_mean_pred,
+                 bs_pred_upper  = bs_list_upper_pred,
+                 bs_pred_median = bs_list_median_pred)
+
+all_pred = bind_cols(sim_dgp, bs_pred, vci_dgp) %>% 
+  ungroup() %>% 
+  dplyr::select(id, seed, data, chem, sim,
+                vci_wa_mean, vci_h_mean, 
+                # For VCI predicted values, take WA*H = mean of Poisson
+                # Take 2.5 and 97.5 percentiles of Poisson(WA*H) with inverse CDF
+                # Scaled vs non-scaled give same predicted value
+                bs_pred_mean, bs_pred_lower,
+                bs_pred_upper, bs_pred_median
+                # For bootstrapped predicted values
+                # Take predicted as WA*H of each bootstrap
+                # Take 2.5 and 97.5 percentiles of bootstrapped distribution (over 150 bootstraps)
+                ) %>% 
+  mutate(vci_pred_mean = map2(vci_wa_mean, vci_h_mean, function(x,y) as.matrix(x) %*% as.matrix(y)),
+         vci_pred_lower = map(vci_pred_mean, qpois, p = 0.025),
+         vci_pred_upper = map(vci_pred_mean, qpois, p = 0.975)) %>% 
+  dplyr::select(-vci_wa_mean, -vci_h_mean)
+
+#### Combine data ####
+see_pred = all_pred %>% 
+  pivot_longer(c(grep("bs_", colnames(.)), grep("vci_", colnames(.))),
+               names_to = c("method", "side", "matrix"),
+               names_sep = "_") %>%
+  mutate(value = map(value, as.matrix)) %>% 
+  pivot_wider(names_from = matrix,
+              values_from = value) %>% 
+  mutate(best = map2(median, mean, function(x,y) if(!is.null(x)) {x} else {y}),
+         err  = map2(chem, best, function(x,y)
+           if (ncol(x) == ncol(y)) {norm(x-y, "F")/norm(x, "F")} else {NA}),
+         iqr  = map2(upper, lower, function(x, y) mean(x-y)),
+         prop_true = pmap(list(chem, lower, upper),
+                     function(x,y,z) sum(x >= y & x <= z)/(nrow(x)*ncol(x)) ),
+         prop_sim = pmap(list(sim, lower, upper),
+                     function(x,y,z) sum(x >= y & x <= z)/(nrow(x)*ncol(x)) )) %>%
+  unnest(c(err, prop_true, prop_sim, iqr))
+
+#### Results Tables ####
+see_pred %>% 
+  group_by(data, method, side) %>%
+  summarise(qs = quantile(prop_true, c(0.25, 0.5, 0.75)), prob = c(0.25, 0.5, 0.75),
+            mean = mean(prop_true),
+            max = max(prop_true),
+            min = min(prop_true)) %>% 
+  pivot_wider(names_from = "prob",
+              values_from = "qs") %>% 
+  dplyr::select(side, data, method, min, `0.25`, `0.5`, mean, `0.75`, max) %>% 
+  arrange(side, data, method)
+
+see_pred %>% 
+  group_by(data, method, side) %>%
+  summarise(qs = quantile(prop_sim, c(0.25, 0.5, 0.75)), prob = c(0.25, 0.5, 0.75),
+            mean = mean(prop_sim),
+            max = max(prop_sim),
+            min = min(prop_sim)) %>% 
+  pivot_wider(names_from = "prob",
+              values_from = "qs") %>% 
+  dplyr::select(side, data, method, min, `0.25`, `0.5`, mean, `0.75`, max) %>% 
+  arrange(side, data, method)
+
+see_pred %>% 
+  group_by(data, method, side) %>% 
+  summarise(qs = quantile(err, c(0.25, 0.5, 0.75)), prob = c(0.25, 0.5, 0.75),
+            mean = mean(err),
+            max = max(err),
+            min = min(err)) %>% 
+  pivot_wider(names_from = "prob",
+              values_from = "qs") %>% 
+  dplyr::select(side, data, method, min, `0.25`, `0.5`, mean, `0.75`, max) %>% 
+  arrange(side, data, method)
+
+see_pred %>% 
+  group_by(data, method, side) %>% 
+  summarise(qs = quantile(iqr, c(0.25, 0.5, 0.75)), prob = c(0.25, 0.5, 0.75),
+            mean = mean(iqr),
+            max = max(iqr),
+            min = min(iqr)) %>% 
+  pivot_wider(names_from = "prob",
+              values_from = "qs") %>% 
+  dplyr::select(side, data, method, min, `0.25`, `0.5`, mean, `0.75`, max) %>% 
+  arrange(side, data, method)
+
+#### Example viz (Single Entry) ####
+#### Loop for each kind of simulation 
+
+#### EWA ####
+plot_pred = tibble()
+prop_pred = tibble()
+
+for (i in 1:3) {
+  truth   = bs_ex[i,]$scores_scaled[[1]][735, 2]
+  v_dist  = as.numeric(bs_ex[i,]$vci_wa_dist[[1]][735, 2,])
+  bs_dist = bs_ex[i,]$bs_wa_dist[[1]][735, 2,]
+  
+  bs_ewa    = bs_ex[i,]$bs_wa_median[[1]][735, 2]
+  bs_wa_25  = bs_ex[i,]$bs_wa_lower[[1]][735, 2]
+  bs_wa_75  = bs_ex[i,]$bs_wa_upper[[1]][735, 2]
+  
+  v_ewa   = as.numeric(bs_ex[i,]$vci_wa_mean[[1]][735, 2])
+  v_wa_25 = as.numeric(bs_ex[i,]$vci_wa_lower[[1]][735, 2])
+  v_wa_75 = as.numeric(bs_ex[i,]$vci_wa_upper[[1]][735, 2])
+  
+  v_prop = sum(bs_ex[i,]$scores_scaled[[1]] <= bs_ex[i,]$vci_wa_upper[[1]] & 
+                 bs_ex[i,]$scores_scaled[[1]] >= bs_ex[i,]$vci_wa_lower[[1]])/4000
+  
+  bs_prop = sum(bs_ex[i,]$scores_scaled[[1]] <= bs_ex[i,]$bs_wa_upper[[1]] & 
+                  bs_ex[i,]$scores_scaled[[1]] >= bs_ex[i,]$bs_wa_lower[[1]])/4000
+  
+  add_plot = tibble(Distribution = v_dist) %>% 
+    mutate(Type = "Variational") %>% 
+    rbind(., tibble(Distribution = bs_dist) %>% 
+            mutate(Type = "Bootstrap"))  %>% 
+    drop_na(.) %>% 
+    mutate(sim_type = str_to_title(type[i]),
+           bs_ewa   = bs_ewa  ,
+           bs_wa_25 = bs_wa_25,
+           bs_wa_75 = bs_wa_75,
+           v_ewa    = v_ewa   ,
+           v_wa_25  = v_wa_25 ,
+           v_wa_75  = v_wa_75,
+           truth = truth)
+  
+  plot_wa = bind_rows(plot_wa, add_plot)
+  prop_wa = bind_rows(prop_wa, bind_cols(bs = bs_prop, v = v_prop))
+}
+rm(list = c("bs_ewa", "bs_wa_75", "bs_wa_25",
+            "v_ewa", "v_wa_25", "v_wa_75", "truth"))
+prop_wa 
+plot_wa %>% dplyr::select(sim_type, bs_ewa:truth) %>% 
+  distinct( )
+
+plot_wa %>% 
+  mutate(sim_type = fct_inorder(sim_type)) %>% 
+  ggplot(aes(x = Distribution, fill = Type)) +
+  geom_rect(aes(xmin = v_wa_25,  xmax = v_wa_75,  ymin=0, ymax=Inf), fill="lightblue", alpha=0.025) +
+  geom_rect(aes(xmin = bs_wa_25, xmax = bs_wa_75, ymin=0, ymax=Inf), fill="pink",      alpha=0.025) +
+  geom_histogram(aes(y=..density..,fill = Type), bins = 100) +
+  #geom_density(aes(group = Type), alpha = 0.5) +
+  scale_fill_manual(values = c("red", "blue")) +
+  theme_bw() + 
+  geom_vline(aes(xintercept = truth),  linetype="dotted", color = "black") +
+  geom_vline(aes(xintercept = bs_ewa), linetype="dotted", color = "yellow") + 
+  geom_vline(aes(xintercept = v_ewa),  linetype="dotted", color = "yellow") + 
+  facet_grid(sim_type~., scales = "free") +
+  theme(legend.position = "bottom",
+        strip.background = element_rect(fill="white")) +
+  ylab("Density")
+
+#### EH ####
+plot_h = tibble()
+prop_h = tibble()
+
+for (i in 1:3) {
+  truth     = bs_ex[i,]$patterns_scaled[[1]][4, 34]
+  v_dist_h  = as.numeric(bs_ex[i,]$vci_h_dist[[1]][4, 34,])
+  bs_dist_h = bs_ex[i,]$bs_h_dist[[1]][4, 34,]
+  
+  bs_eh    = bs_ex[i,]$bs_h_median[[1]][4, 34]
+  bs_h_25  = bs_ex[i,]$bs_h_lower[[1]][4, 34]
+  bs_h_75  = bs_ex[i,]$bs_h_upper[[1]][4, 34]
+  
+  v_eh   = as.numeric(bs_ex[i,]$vci_h_mean[[1]][4, 34])
+  v_h_25 = as.numeric(bs_ex[i,]$vci_h_lower[[1]][4, 34])
+  v_h_75 = as.numeric(bs_ex[i,]$vci_h_upper[[1]][4, 34])
+  
+  v_prop = sum(bs_ex[i,]$patterns_scaled[[1]] <= bs_ex[i,]$vci_h_upper[[1]] & 
+                 bs_ex[i,]$patterns_scaled[[1]] >= bs_ex[i,]$vci_h_lower[[1]])/4000
+  
+  bs_prop = sum(bs_ex[i,]$patterns_scaled[[1]] <= bs_ex[i,]$bs_h_upper[[1]] & 
+                  bs_ex[i,]$patterns_scaled[[1]] >= bs_ex[i,]$bs_h_lower[[1]])/4000
+  
+  add_plot = tibble(Distribution = v_dist_h) %>% 
+    mutate(Type = "Variational") %>% 
+    rbind(., tibble(Distribution = bs_dist_h) %>% 
+            mutate(Type = "Bootstrap"))  %>% 
+    drop_na(.) %>% 
+    mutate(sim_type = str_to_title(type[i]),
+           bs_eh   = bs_eh  ,
+           bs_h_25 = bs_h_25,
+           bs_h_75 = bs_h_75,
+           v_eh    = v_eh   ,
+           v_h_25  = v_h_25 ,
+           v_h_75  = v_h_75,
+           truth = truth)
+  
+  plot_h = bind_rows(plot_h, add_plot)
+  prop_h = bind_rows(prop_h, bind_cols(bs = bs_prop, v = v_prop))
+}
+rm(list = c("bs_eh", "bs_h_75", "bs_h_25",
+            "v_eh", "v_h_25", "v_h_75", "truth"))
+prop_h 
+plot_h %>% dplyr::select(sim_type, bs_eh:truth) %>% 
+  distinct( )
+
+plot_h %>% 
+  mutate(sim_type = fct_inorder(sim_type)) %>% 
+  ggplot(aes(x = Distribution, fill = Type)) +
+  geom_rect(aes(xmin = bs_h_25, xmax = bs_h_75, ymin=0, ymax=Inf), fill="pink",      alpha=0.025) +
+  geom_rect(aes(xmin = v_h_25,  xmax = v_h_75,  ymin=0, ymax=Inf), fill="lightblue", alpha=0.025) +
+  geom_histogram(aes(y=..density..,fill = Type), bins = 100) +
+  #geom_density(aes(group = Type), alpha = 0.5) +
+  scale_fill_manual(values = c("red", "blue")) +
+  theme_bw() + 
+  geom_vline(aes(xintercept = truth),  linetype="dotted", color = "black") +
+  geom_vline(aes(xintercept = bs_eh), linetype="dotted", color = "yellow") + 
+  geom_vline(aes(xintercept = v_eh),  linetype="dotted", color = "yellow") + 
+  facet_grid(sim_type~., scales = "free") +
+  theme(legend.position = "bottom",
+        strip.background = element_rect(fill="white")) +
+  ylab("Density")
+
+
+
+
+
+
+
