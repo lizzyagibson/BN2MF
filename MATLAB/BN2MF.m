3f2c4d8128533b8c5a7fc0c8a45de18613e53dd1
lizzyagibson <eag2186@cumc.columbia.edu>
2021-02-22 13:30:39 -0500

    Does a less sparse prior change things?

--- MATLAB/BN2MF.m
+++ MATLAB/BN2MF.m
@@ -68,7 +68,7 @@ for iter = 1:num_iter
     A1 = 1 + (a01 + sum(sum(X_reshape.*P,3),2)' - 1)/T;
     A2 = (a02 + sum(W1./W2,1).*sum(H1./H2,2)')/T; 
     
-    idx_prune = find(A1./A2 < 10^-3);
+    idx_prune = find(A1./A2 < 10^-4);
     if ~isempty(idx_prune)
       W1(:,idx_prune) = [];
       W2(:,idx_prune) = [];
@@ -88,7 +88,7 @@ for iter = 1:num_iter
 end
 
 end_score(i) = score(find(score,1,'last'));  
-%disp(['Run Number: ' num2str(i) '. Iter Number: ' num2str(iter) '. Iter Score: ' num2str(end_score(i))]); 
+disp(['Run Number: ' num2str(i) '. Iter Number: ' num2str(iter) '. Iter Score: ' num2str(end_score(i))]); 
 
 % Among the results, use the fitted variational parameters that achieve the HIGHEST ELBO
 if i == 1 || (i > 1 && (end_score(i) >= max(end_score)))
@@ -108,4 +108,4 @@ end
 
 end
 
-disp(['Final Iter: ' num2str(final_iter), '. Final Iter Number: ' num2str(finalscore)]); 
+disp(['Final Iter: ' num2str(final_iter), '. Final Iter Score: ' num2str(finalscore)]); 
